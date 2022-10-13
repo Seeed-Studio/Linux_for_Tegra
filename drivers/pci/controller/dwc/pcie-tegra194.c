dd6f313cf3f69155676741f922523b2d04d905f3
Vidya Sagar <vidyas@nvidia.com>
2022-10-14 00:08:43 +0530

    NVIDIA: SAUCE: PCI: tegra194: Free resources during controller deinitialization
    BugLink: https://bugs.launchpad.net/bugs/2072591
    
    Call dw_pcie_ep_deinit() during controller deinitialization to free the
    resources allocated in common DesignWare driver.
    
    Signed-off-by: Vidya Sagar <vidyas@nvidia.com>
    Reviewed-by: Nagarjuna Kristam <nkristam@nvidia.com>
    Reviewed-by: Bitan Biswas <bbiswas@nvidia.com>
    Tested-by: Nagarjuna Kristam <nkristam@nvidia.com>
    Signed-off-by: Laxman Dewangan <ldewangan@nvidia.com>
    Acked-by: Jacob Martin <jacob.martin@canonical.com>
    Acked-by: Noah Wager <noah.wager@canonical.com>
    Signed-off-by: Noah Wager <noah.wager@canonical.com>

--- drivers/pci/controller/dwc/pcie-tegra194.c
+++ drivers/pci/controller/dwc/pcie-tegra194.c
@@ -1683,11 +1683,15 @@ fail_pm_get_sync:
 
 static void pex_ep_event_pex_rst_assert(struct tegra_pcie_dw *pcie)
 {
+	struct dw_pcie *pci = &pcie->pci;
+	struct dw_pcie_ep *ep = &pci->ep;
 	u32 val;
 	int ret;
 
 	if (pcie->ep_state == EP_STATE_DISABLED)
 		return;
+
+	dw_pcie_ep_deinit_notify(ep);
 
 	ret = readl_poll_timeout(pcie->appl_base + APPL_DEBUG, val,
 		((val & APPL_DEBUG_LTSSM_STATE_MASK) == LTSSM_STATE_DETECT_QUIET) ||
