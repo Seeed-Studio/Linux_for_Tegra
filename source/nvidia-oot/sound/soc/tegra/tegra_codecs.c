389957ba109f96ac6131031777e140d45d288214
Sameer Pujar <spujar@nvidia.com>
2022-10-17 17:48:34 +0000

    ASoC: tegra: Fix build errors with kstable
    Build errors seen in following contexts with kstable:
    
      - The 'non_legacy_dai_naming' flag is not available in struct
        'snd_soc_component_driver' in v6.0. Instead it is replaced
        with flag 'legacy_dai_naming' and default value works fine.
        To fix build error on kstable exclude the removed flag for
        kernel v6.0 onward.
    
      - Signature of snd_soc_card_jack_new() has changed in v5.19
        which drops struct 'snd_soc_jack_pins' related members. These
        were unused for Tegra and is safe to just update the funcion
        call. Fix this by using correct function signature based on
        kernel version checks.
    
    With above fixed now, remove workaround from Makefile and enable
    Audio OOT driver builds again with kstable.
    
    Bug 3831575
    
    Change-Id: I7b65c89e8140f6e085528fb827d47c3909233db1
    Signed-off-by: Sameer Pujar <spujar@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/c/linux-nv-oot/+/2793430
    Reviewed-by: Jonathan Hunter <jonathanh@nvidia.com>
    Reviewed-by: Laxman Dewangan <ldewangan@nvidia.com>
    GVS: Gerrit_Virtual_Submit <buildbot_gerritrpt@nvidia.com>

--- source/nvidia-oot/sound/soc/tegra/tegra_codecs.c
+++ source/nvidia-oot/sound/soc/tegra/tegra_codecs.c
@@ -6,6 +6,7 @@
 
 #include <dt-bindings/sound/tas2552.h>
 #include <linux/input.h>
+#include <linux/version.h>
 #include <sound/jack.h>
 #include <sound/soc.h>
 #include <sound/simple_card_utils.h>
@@ -43,8 +44,13 @@ static int tegra_machine_rt56xx_init(struct snd_soc_pcm_runtime *rtd)
 	if (!jack)
 		return -ENOMEM;
 
+#if (KERNEL_VERSION(5, 19, 0) > LINUX_VERSION_CODE)
 	err = snd_soc_card_jack_new(card, "Headset Jack", SND_JACK_HEADSET,
 				    jack, NULL, 0);
+#else
+	err = snd_soc_card_jack_new(card, "Headset Jack", SND_JACK_HEADSET,
+				    jack);
+#endif
 	if (err) {
 		dev_err(card->dev, "Headset Jack creation failed %d\n", err);
 		return err;
