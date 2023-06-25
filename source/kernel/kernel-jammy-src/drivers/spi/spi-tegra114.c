5e2d8dc63479bd53e5ffbd6646c19fb506cccdd2
Vishwaroop A <va@nvidia.com>
2023-06-25 04:16:34 +0000

    UBUNTU: SAUCE: drivers: spi: add support for prod framework
    BugLink: https://bugs.launchpad.net/bugs/2028400
    
    Write production settings through prod framework for
    spi controller driver.
    
    Signed-off-by: Vishwaroop A <va@nvidia.com>
    Reviewed-by: Laxman Dewangan <ldewangan@nvidia.com>
    Signed-off-by: Abhilash G <abhilashg@nvidia.com>
    Acked-by: Jacob Martin <jacob.martin@canonical.com>
    Acked-by: Ian May <ian.may@canonical.com>
    Signed-off-by: Jacob Martin <jacob.martin@canonical.com>

--- source/kernel/kernel-jammy-src/drivers/spi/spi-tegra114.c
+++ source/kernel/kernel-jammy-src/drivers/spi/spi-tegra114.c
@@ -23,6 +23,7 @@
 #include <linux/of_device.h>
 #include <linux/reset.h>
 #include <linux/spi/spi.h>
+#include <linux/tegra_prod.h>
 
 #define SPI_COMMAND1				0x000
 #define SPI_BIT_LENGTH(x)			(((x) & 0x1f) << 0)
@@ -219,6 +220,7 @@ struct tegra_spi_data {
 	dma_addr_t				tx_dma_phys;
 	struct dma_async_tx_descriptor		*tx_dma_desc;
 	const struct tegra_spi_soc_data		*soc_data;
+	struct tegra_prod			*prod_list;
 };
 
 static int tegra_spi_runtime_suspend(struct device *dev);
@@ -717,6 +719,23 @@ static void tegra_spi_deinit_dma_param(struct tegra_spi_data *tspi,
 	dma_release_channel(dma_chan);
 }
 
+static void tegra_spi_set_prod(struct tegra_spi_data *tspi, int cs)
+{
+	int ret;
+	char prod_name[15];
+
+	/* Avoid write to register for transfers to last used device */
+	if (tspi->last_used_cs == cs)
+		return;
+
+	ret = tegra_prod_set_by_name(&tspi->base, "prod", tspi->prod_list);
+	sprintf(prod_name, "prod_c_cs%d", cs);
+	ret = tegra_prod_set_by_name(&tspi->base, prod_name, tspi->prod_list);
+	if (ret)
+		dev_dbg(tspi->dev, "prod settings failed with error %d", ret);
+	tspi->last_used_cs = cs;
+}
+
 static int tegra_spi_set_hw_cs_timing(struct spi_device *spi)
 {
 	struct tegra_spi_data *tspi = spi_master_get_devdata(spi->master);
@@ -783,6 +802,8 @@ static u32 tegra_spi_setup_transfer_one(struct spi_device *spi,
 	u8 bits_per_word = t->bits_per_word;
 	u32 command1, command2;
 	int req_mode;
+	int ret;
+	char prod_name[15];
 	u32 tx_tap = 0, rx_tap = 0;
 
 	if (speed != tspi->cur_speed) {
@@ -846,16 +867,20 @@ static u32 tegra_spi_setup_transfer_one(struct spi_device *spi,
 				command1 &= ~SPI_CS_SW_VAL;
 		}
 
-		if (tspi->last_used_cs != spi->chip_select) {
-			if (cdata && cdata->tx_clk_tap_delay)
-				tx_tap = cdata->tx_clk_tap_delay;
-			if (cdata && cdata->rx_clk_tap_delay)
-				rx_tap = cdata->rx_clk_tap_delay;
-			command2 = SPI_TX_TAP_DELAY(tx_tap) |
-				   SPI_RX_TAP_DELAY(rx_tap);
-			if (command2 != tspi->def_command2_reg)
-				tegra_spi_writel(tspi, command2, SPI_COMMAND2);
-			tspi->last_used_cs = spi->chip_select;
+		if (!tspi->prod_list) {
+			if (tspi->last_used_cs != spi->chip_select) {
+				if (cdata && cdata->tx_clk_tap_delay)
+					tx_tap = cdata->tx_clk_tap_delay;
+				if (cdata && cdata->rx_clk_tap_delay)
+					rx_tap = cdata->rx_clk_tap_delay;
+				command2 = SPI_TX_TAP_DELAY(tx_tap) |
+					   SPI_RX_TAP_DELAY(rx_tap);
+				if (command2 != tspi->def_command2_reg)
+					tegra_spi_writel(tspi, command2, SPI_COMMAND2);
+				tspi->last_used_cs = spi->chip_select;
+			}
+		} else {
+			tegra_spi_set_prod(tspi, spi->chip_select);
 		}
 
 	} else {
@@ -1334,6 +1359,11 @@ static int tegra_spi_probe(struct platform_device *pdev)
 
 	tspi->master = master;
 	tspi->dev = &pdev->dev;
+	tspi->prod_list = devm_tegra_prod_get(tspi->dev);
+	if (IS_ERR(tspi->prod_list)) {
+		dev_dbg(&pdev->dev, "Prod settings list not initialized\n");
+		tspi->prod_list = NULL;
+	}
 	spin_lock_init(&tspi->lock);
 
 	tspi->soc_data = of_device_get_match_data(&pdev->dev);
@@ -1404,12 +1434,14 @@ static int tegra_spi_probe(struct platform_device *pdev)
 	reset_control_assert(tspi->rst);
 	udelay(2);
 	reset_control_deassert(tspi->rst);
+	tspi->last_used_cs = master->num_chipselect + 1;
+	/* initialize CS with 0 in probe */
+	tegra_spi_set_prod(tspi, 0);
 	tspi->def_command1_reg  = SPI_M_S;
 	tegra_spi_writel(tspi, tspi->def_command1_reg, SPI_COMMAND1);
 	tspi->spi_cs_timing1 = tegra_spi_readl(tspi, SPI_CS_TIMING1);
 	tspi->spi_cs_timing2 = tegra_spi_readl(tspi, SPI_CS_TIMING2);
 	tspi->def_command2_reg = tegra_spi_readl(tspi, SPI_COMMAND2);
-	tspi->last_used_cs = master->num_chipselect + 1;
 	pm_runtime_put(&pdev->dev);
 	ret = request_threaded_irq(tspi->irq, tegra_spi_isr,
 				   tegra_spi_isr_thread, IRQF_ONESHOT,
