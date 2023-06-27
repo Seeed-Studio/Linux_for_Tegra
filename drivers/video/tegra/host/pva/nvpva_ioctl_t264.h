569844dd8340a180ed2e835323410ee4d65ac71f
abhinayaa <abhinayaa@nvidia.com>
2023-06-27 01:04:31 +0000

    drivers: pva: Update HW SEQ and ADB for T26x
    - T26x support 304 ADBs compared to 272 on T23x. Add code to support
    this distinction.
    
    - T26x introduces the following updates to HWSEQ:
    	- a new HWSEQ mode - Random Region Access (RRA).
    	- increased HWSEQ RAM size of 2KB.
      Add code to support these changes.
    
    Jira PVAAS-13252
    
    Change-Id: I92d65524b526eda1be8f8dc81dc4e41d0db7f488
    Signed-off-by: abhinayaa <abhinayaa@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/c/linux-t264/+/2926761
    Reviewed-by: Sreehari Mohan <sreeharim@nvidia.com>
    Reviewed-by: Amruta Sai Anusha Bhamidipati <abhamidipati@nvidia.com>
    Reviewed-by: Omar Nemri <onemri@nvidia.com>
    GVS: Gerrit_Virtual_Submit <buildbot_gerritrpt@nvidia.com>

--- drivers/video/tegra/host/pva/nvpva_ioctl_t264.h
+++ drivers/video/tegra/host/pva/nvpva_ioctl_t264.h
@@ -37,5 +37,37 @@
  * R5 FW reserves one DMA channel for internal use.
  */
 #define NVPVA_TASK_MAX_DMA_CHANNELS_T26X (15U)
+/* NOTE: This is a re-definition of nvpva_dma_channel that
+ * contains T26x specific changes. Once T26x is public,
+ * this definition may be merged nvpva_dma_channel.
+ *
+ * Also note that the flags1 field has the following flags:
+ * - MSB for the HW Sequencer start index field in channel registers
+ *  	DMA_CHANNEL_HWSEQCNTL[1].bit[0] = flags1[0].bit[0];
+ * - MSB for the HW Sequencer end index field in channel registers
+ *	DMA_CHANNEL_HWSEQCNTL[2].bit[4] = flags1[0].bit[2];
+ */
+struct nvpva_dma_channel_ex {
+	uint8_t descIndex;
+	uint8_t blockHeight;
+	uint16_t adbSize;
+	uint8_t vdbSize;
+	uint16_t adbOffset;
+	uint8_t vdbOffset;
+	uint32_t outputEnableMask;
+	uint32_t padValue;
+	uint8_t reqPerGrant;
+	uint8_t prefetchEnable;
+	uint8_t chRepFactor;
+	uint8_t hwseqStart;
+	uint8_t hwseqEnd;
+	uint8_t hwseqEnable;
+	uint8_t hwseqTraversalOrder;
+	uint8_t hwseqTxSelect;
+	uint8_t hwseqTriggerDone;
+	uint8_t hwseqFrameCount;
+	uint8_t hwseqConFrameSeq;
+	uint8_t flags1;
+};
 
 #endif /* __NVPVA_IOCTL_T264_H__ */
