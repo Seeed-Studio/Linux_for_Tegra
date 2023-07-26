f14d861dacd517087b2ddf96f1c3163b67cc0949
Sumit Gupta <sumitg@nvidia.com>
2023-07-26 17:34:00 +0530

    NVIDIA: SAUCE: soc: tegra: cbb: clear error force register with error status
    Clearing error force register bits in addition to the error
    status register. Not clearing this register results in getting
    repeated error interrupts on error injection with force register.
    
    http://nvbugs/4136792
    
    Signed-off-by: Sumit Gupta <sumitg@nvidia.com>
    Signed-off-by: Laxman Dewangan <ldewangan@nvidia.com>
    Acked-by: Noah Wager <noah.wager@canonical.com>
    Acked-by: Jacob Martin <jacob.martin@canonical.com>
    Signed-off-by: Noah Wager <noah.wager@canonical.com>

--- drivers/soc/tegra/cbb/tegra234-cbb.c
+++ drivers/soc/tegra/cbb/tegra234-cbb.c
@@ -187,7 +187,10 @@ static void tegra234_cbb_error_clear(struct tegra_cbb *cbb)
 {
 	struct tegra234_cbb *priv = to_tegra234_cbb(cbb);
 
+	writel(0, priv->mon + FABRIC_MN_MASTER_ERR_FORCE_0);
+
 	writel(priv->fabric->err_status_clr, priv->mon + FABRIC_MN_MASTER_ERR_STATUS_0);
+
 	dsb(sy);
 }
 
