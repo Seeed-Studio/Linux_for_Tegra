4c575fd98cd37e9ef1424166e24c7c7228d60d9f
Laxman Dewangan <ldewangan@nvidia.com>
2023-11-07 15:02:57 +0000

    firmware: Add dummy tegra BPMP driver for Linux 6.6
    Tegra BPMP driver is not supported with Linux 6.6. However,
    the packaging of the embedded-linux required this driver.
    Add dummy driver to complete the packaging till packaging
    files have solution to complete with and without this
    driver.
    
    Bug 4346767
    
    Change-Id: I113019d6ef25860511683b326f9c90d6ff1e99a3
    Signed-off-by: Laxman Dewangan <ldewangan@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/c/linux-nv-oot/+/3012092
    GVS: Gerrit_Virtual_Submit <buildbot_gerritrpt@nvidia.com>

---
+++ drivers/firmware/tegra/tegra-bpmp-dummy.c
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: GPL-2.0
+/*
+ * Copyright (c) 2023, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+ */
+
+#include <linux/module.h>
+
+/* Dummy implementation for module */
+static int __init tegra_bpmp_dummy_init(void)
+{
+	return 0;
+}
+device_initcall(tegra_bpmp_dummy_init);
+
+MODULE_AUTHOR("Laxman Dewangan <ldewangan@nvidia.com>");
+MODULE_DESCRIPTION("Dummy bpmp driver");
+MODULE_LICENSE("GPL V2");
