af936f3b1eb2bad44334c1682167b01f70e1af2e
Laxman Dewangan <ldewangan@nvidia.com>
2023-11-10 02:28:33 +0000

    tegra_hv: Enable driver for Linux 6.2 and above
    The driver tegra_hv was disabled for the Linux 6.2 and above
    due to build failure. All build issue is fixed and re-enabling
    driver for all kernel.
    
    Bug 4370594
    
    Change-Id: I9edef01a4f204b138204d583d9f7f61dc6112a25
    Signed-off-by: Laxman Dewangan <ldewangan@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/c/linux-nv-oot/+/3014191
    Reviewed-by: Bitan Biswas <bbiswas@nvidia.com>
    GVS: Gerrit_Virtual_Submit <buildbot_gerritrpt@nvidia.com>

--- include/soc/tegra/virt/hv-ivc.h
+++ include/soc/tegra/virt/hv-ivc.h
@@ -32,7 +32,7 @@ struct tegra_hv_ivm_cookie {
 	void *reserved;
 };
 
-#if (KERNEL_VERSION(6, 2, 0) > LINUX_VERSION_CODE) && defined(CONFIG_TEGRA_VIRTUALIZATION)
+#if defined(CONFIG_TEGRA_VIRTUALIZATION)
 bool is_tegra_hypervisor_mode(void);
 
 /**
@@ -451,8 +451,6 @@ static inline struct tegra_ivc *tegra_hv_ivc_convert_cookie(
 {
 	return ERR_PTR(-ENOTSUPP);
 };
-#endif /* (KERNEL_VERSION(6, 2, 0) > LINUX_VERSION_CODE) &&
-	* defined(CONFIG_TEGRA_VIRTUALIZATION)
-	*/
+#endif /* defined(CONFIG_TEGRA_VIRTUALIZATION) */
 
 #endif /* __TEGRA_HV_IVC_H */
