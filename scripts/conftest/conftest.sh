2a9ce8d0e20d8864e9ae8b411d54a8e4f176b12d
Bitan Biswas <bbiswas@nvidia.com>
2023-12-01 13:02:15 +0000

    crypto: conftest struct crypto_engine_ctx support
    struct crypto_engine_ctx removed in k6.6 hence use
    conftest helper based macro to select updated data types
    
    Bug 4346767
    
    Change-Id: I23e8f054719f399877b11f42d355f940d8e07186
    Signed-off-by: Bitan Biswas <bbiswas@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/c/linux-nv-oot/+/3026513
    Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>
    Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>

--- scripts/conftest/conftest.sh
+++ scripts/conftest/conftest.sh
@@ -7474,6 +7474,24 @@ compile_test() {
             compile_check_conftest "$CODE" "NV_V4L2_ASYNC_SUBDEV_NF_INIT_PRESENT" "" "functions"
         ;;
 
+        crypto_engine_ctx_struct_removed_test)
+            #
+            # Determine if struct 'crypto_engine_ctx' is removed in linux kernel.
+            #
+            # Commit 5ce0bc68e0ee ("crypto: engine - Remove crypto_engine_ctx")
+            # Linux v6.6 removed struct crypto_engine_ctx
+            #
+            CODE="
+            #include <crypto/engine.h>
+            void conftest_crypto_engine_ctx_struct_removed_test(void) {
+                    struct crypto_engine_ctx *ptr = NULL;
+                    struct crypto_engine_ctx enginectx;
+                    ptr = &enginectx;
+            }"
+
+            compile_check_conftest "$CODE" "NV_CONFTEST_REMOVE_STRUCT_CRYPTO_ENGINE_CTX" "" "functions"
+        ;;
+
         # When adding a new conftest entry, please use the correct format for
         # specifying the relevant upstream Linux kernel commit.
         #
