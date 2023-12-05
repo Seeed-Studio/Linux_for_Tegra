21a661c58ffddf32bbba9ebd91d05b70ab3764b1
Joshua Cha <joshuac@nvidia.com>
2023-12-05 22:57:53 +0900

    nvsciipc: fix mixed license
    Bug 4405013
    
    Change-Id: I4c37c89b7dcc4c25e0b4e568b8d68d1a73c424f7
    Signed-off-by: Joshua Cha <joshuac@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/c/linux-nv-oot/+/3028570
    Reviewed-by: Kurt Yi <kyi@nvidia.com>
    Reviewed-by: Jonathan Hunter <jonathanh@nvidia.com>
    GVS: Gerrit_Virtual_Submit <buildbot_gerritrpt@nvidia.com>

--- drivers/virt/tegra/ivc-cdev.c
+++ drivers/virt/tegra/ivc-cdev.c
@@ -1,13 +1,6 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 /*
  * SPDX-FileCopyrightText: Copyright (c) 2022-2023 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
- *
- * NVIDIA CORPORATION, its affiliates and licensors retain all intellectual
- * property and proprietary rights in and to this material, related
- * documentation and any modifications thereto. Any use, reproduction,
- * disclosure or distribution of this material and related documentation
- * without an express license agreement from NVIDIA CORPORATION or
- * its affiliates is strictly prohibited.
  */
 
 #include <nvidia/conftest.h>
