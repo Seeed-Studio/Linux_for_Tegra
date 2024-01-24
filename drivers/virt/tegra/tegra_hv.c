288de33b625ffd03f45cd6da68ff192dfde2ae55
Laxman Dewangan <ldewangan@nvidia.com>
2024-01-24 11:34:45 +0000

    tegra_hv: Fix missing headers for Linux v6.8
    For Linux v6.8, commit ef175b29a242 ("of: Stop circularly including
    of_device.h and of_platform.h") updated the OF headers included by these
    header files. This breaks the build for various drivers and so fix this
    by including the headers that are actually needed for each driver.
    
    Bug 4448428
    
    Change-Id: I19a0d9003aa655d12c8ae50131f15a6ee4cc59f8
    Signed-off-by: Laxman Dewangan <ldewangan@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/c/linux-nv-oot/+/3063634
    GVS: Gerrit_Virtual_Submit <buildbot_gerritrpt@nvidia.com>

--- drivers/virt/tegra/tegra_hv.c
+++ drivers/virt/tegra/tegra_hv.c
@@ -12,7 +12,6 @@
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/device.h>
-#include <linux/of_platform.h>
 #include <linux/slab.h>
 #include <linux/seq_file.h>
 #include <linux/spinlock.h>
@@ -20,8 +19,9 @@
 #include <linux/sched.h>
 #include <linux/interrupt.h>
 #include <linux/of.h>
-#include <linux/module.h>
-#include <linux/of.h>
+#include <linux/of_device.h>
+#include <linux/of_platform.h>
+#include <linux/platform_device.h>
 #include <linux/of_irq.h>
 
 #include <soc/tegra/virt/syscalls.h>
