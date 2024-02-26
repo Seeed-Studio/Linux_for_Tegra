bec1d86778b921dc3ae0ef3552236c4a01e3ee8a
Bhadram Varka <vbhadram@nvidia.com>
2024-02-26 11:19:49 +0530

    nvethernet: fix build issues with safety
    Bug 4506095
    
    Change-Id: Ia24590ab716d7f879ca5394b7a5512db072c4fdd
    Signed-off-by: Bhadram Varka <vbhadram@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/c/linux-nv-oot/+/3083506
    Reviewed-by: Narayana Reddy P <narayanr@nvidia.com>
    Reviewed-by: Srinivas Ramachandran <srinivasra@nvidia.com>
    GVS: Gerrit_Virtual_Submit <buildbot_gerritrpt@nvidia.com>

--- drivers/net/ethernet/nvidia/nvethernet/sysfs.c
+++ drivers/net/ethernet/nvidia/nvethernet/sysfs.c
@@ -2755,10 +2755,10 @@ static struct attribute *ether_sysfs_attrs_without_macsec[] = {
 	&dev_attr_nvgro_stats.attr,
 	&dev_attr_nvgro_dump.attr,
 #endif
-#endif /* OSI_STRIPPED_LIB */
 #ifdef HSI_SUPPORT
 	&dev_attr_hsi_enable.attr,
 #endif
+#endif /* OSI_STRIPPED_LIB */
 	NULL
 };
 
@@ -3353,9 +3353,9 @@ static void ether_remove_debugfs(struct ether_priv_data *pdata)
 int ether_sysfs_register(struct ether_priv_data *pdata)
 {
 	struct device *dev = pdata->dev;
+	int ret = 0;
 #ifndef OSI_STRIPPED_LIB
 #ifdef CONFIG_DEBUG_FS
-	int ret = 0;
 
 	if (debugfs_initialized()) {
 		ret = ether_create_debugfs(pdata);
