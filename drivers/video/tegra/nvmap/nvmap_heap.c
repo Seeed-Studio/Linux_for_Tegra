b451ef3baf7139862fb8cf83ea9c772732bce08b
Yash Bhatt <ybhatt@nvidia.com>
2024-04-05 03:44:35 +0000

    video: tegra: nvmap: Deprecate dma_dev_from_handle
    Deprecate dma_dev_from_handle from nvmap as it has no caller.
    
    Bug 4479027
    
    Change-Id: Id3b3e400a686e61366929d63ce6a2ef76c251edb
    Signed-off-by: Yash Bhatt <ybhatt@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/c/linux-nv-oot/+/3109957
    Reviewed-by: Pritesh Raithatha <praithatha@nvidia.com>
    Reviewed-by: Ketan Patil <ketanp@nvidia.com>
    GVS: Gerrit_Virtual_Submit <buildbot_gerritrpt@nvidia.com>
    Reviewed-by: Ashish Mhetre <amhetre@nvidia.com>
    Reviewed-by: Sachin Nikam <snikam@nvidia.com>

--- drivers/video/tegra/nvmap/nvmap_heap.c
+++ drivers/video/tegra/nvmap/nvmap_heap.c
@@ -40,22 +40,6 @@
 
 static struct kmem_cache *heap_block_cache;
 
-struct device *dma_dev_from_handle(unsigned long type)
-{
-	int i;
-	struct nvmap_carveout_node *co_heap;
-
-	for (i = 0; i < nvmap_dev->nr_carveouts; i++) {
-		co_heap = &nvmap_dev->heaps[i];
-
-		if (!(co_heap->heap_bit & type))
-			continue;
-
-		return co_heap->carveout->dma_dev;
-	}
-	return ERR_PTR(-ENODEV);
-}
-
 int nvmap_query_heap_peer(struct nvmap_heap *heap, unsigned int *peer)
 {
 	if (!heap || !heap->is_ivm)
