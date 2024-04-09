cfda14f180f12b893553d610e30c31be0b96762b
Yijun Zhou <yijunz@nvidia.com>
2024-04-09 11:10:17 +0000

    bootargs: move common androidboot. param to bootconfig
    Remove common androidboot.<param> from cmdline and set "bootconfig".
    
    Bug 4256908
    
    Change-Id: I5f77bc3645ec888c8272bc31f923b854b82f2340
    Reviewed-on: https://git-master.nvidia.com/r/c/device/hardware/nvidia/t23x-public-dts/+/3112136
    Reviewed-by: Laxman Dewangan <ldewangan@nvidia.com>
    Reviewed-by: Yijun Zhou <yijunz@nvidia.com>
    Reviewed-by: svc-sw-mobile-l4t <svc-sw-mobile-l4t@nvidia.com>
    Reviewed-by: Ankita Garg <ankitag@nvidia.com>
    GVS: buildbot_gerritrpt <buildbot_gerritrpt@nvidia.com>
    Tested-by: Yijun Zhou <yijunz@nvidia.com>

--- include/platforms/dt-bindings/tegra234-android-bootargs.h
+++ include/platforms/dt-bindings/tegra234-android-bootargs.h
@@ -12,11 +12,15 @@
 // Definitions for tegra234 android bootargs
 
 // Make sure ANDROID_BOOTARGS and ANDROID_KDUMP_BOOTARGS are consistent except the later has "crashkernel=512M enforcing=0 androidboot.selinux=permissive" appended
-#define ANDROID_BOOTARGS       "androidboot.boot_devices=bus@0/3460000.mmc console=ttyTCU0,115200 rootfstype=ext4 mminit_loglevel=4 androidboot.slot_suffix=_a androidboot.hypervisor=disabled androidboot.verifiedbootstate=orange androidboot.xudc=3550000.usb androidboot.hardware=t234ref loop.max_part=7 firmware_class.path=/vendor/firmware"
+#define ANDROID_BOOTARGS       "bootconfig console=ttyTCU0,115200 rootfstype=ext4 mminit_loglevel=4 loop.max_part=7 firmware_class.path=/vendor/firmware"
 
-#define ANDROID_KDUMP_BOOTARGS "androidboot.boot_devices=bus@0/3460000.mmc console=ttyTCU0,115200 rootfstype=ext4 mminit_loglevel=4 androidboot.slot_suffix=_a androidboot.hypervisor=disabled androidboot.verifiedbootstate=orange androidboot.xudc=3550000.usb androidboot.hardware=t234ref loop.max_part=7 firmware_class.path=/vendor/firmware crashkernel=512M enforcing=0 androidboot.selinux=permissive"
+#define ANDROID_KDUMP_BOOTARGS "bootconfig console=ttyTCU0,115200 rootfstype=ext4 mminit_loglevel=4 isabled loop.max_part=7 firmware_class.path=/vendor/firmware crashkernel=512M enforcing=0 androidboot.selinux=permissive"
+
+#define ANDROID_BOOTCONFIG "androidboot.boot_devices=bus@0/3460000.mmc\nandroidboot.hypervisor=disabled\nandroidboot.xudc=3550000.usb\nandroidboot.hardware=t234ref\n"
 
 // Make sure ANDROID_FIRESPRAY_BOOTARGS and ANDROID_FIRESPRAY_KDUMP_BOOTARGS are consistent except the later has "crashkernel=512M" appended
-#define ANDROID_FIRESPRAY_BOOTARGS       "androidboot.boot_devices=bus@0/3460000.mmc console=ttyTCU0,115200 rootfstype=ext4 mminit_loglevel=4 enforcing=0 androidboot.selinux=permissive androidboot.slot_suffix=_a androidboot.hypervisor=disabled androidboot.verifiedbootstate=orange androidboot.xudc=3550000.usb androidboot.hardware=t234ref loop.max_part=7 firmware_class.path=/vendor/firmware androidboot.serialno=1423522020643"
+#define ANDROID_FIRESPRAY_BOOTARGS       "bootconfig console=ttyTCU0,115200 rootfstype=ext4 mminit_loglevel=4 enforcing=0 loop.max_part=7 firmware_class.path=/vendor/firmware"
 
-#define ANDROID_FIRESPRAY_KDUMP_BOOTARGS "androidboot.boot_devices=bus@0/3460000.mmc console=ttyTCU0,115200 rootfstype=ext4 mminit_loglevel=4 enforcing=0 androidboot.selinux=permissive androidboot.slot_suffix=_a androidboot.hypervisor=disabled androidboot.verifiedbootstate=orange androidboot.xudc=3550000.usb androidboot.hardware=t234ref loop.max_part=7 firmware_class.path=/vendor/firmware androidboot.serialno=1423522020643 crashkernel=512M"
+#define ANDROID_FIRESPRAY_KDUMP_BOOTARGS "bootconfig console=ttyTCU0,115200 rootfstype=ext4 mminit_loglevel=4 enforcing=0 loop.max_part=7 firmware_class.path=/vendor/firmware crashkernel=512M"
+
+#define ANDROID_FIRESPRAY_BOOTCONFIG "androidboot.boot_devices=bus@0/3460000.mmc\nandroidboot.hypervisor=disabled\nandroidboot.xudc=3550000.usb\nandroidboot.hardware=t234ref\nandroidboot.selinux=permissive\nandroidboot.serialno=1423522020643\n"
