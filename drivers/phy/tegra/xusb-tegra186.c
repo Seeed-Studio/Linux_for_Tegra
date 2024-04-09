8ab915e17e9c1bb30def2833ff736fd1546cef2b
haotienh <haotienh@nvidia.com>
2024-04-09 06:08:12 +0000

    NVIDIA: SAUCE: phy: xusb: Disable USB2 tracking for T234
    BugLink: https://bugs.launchpad.net/bugs/2080908
    
    Disabling USB2 periodic tracking for T234 to fix USB babble errors,
    which happen when PCIe NVMe SSD, LPDDR and USB2 devices are working
    simultaneously.
    
    http://nvbugs/4309470
    
    Signed-off-by: haotienh <haotienh@nvidia.com>
    Signed-off-by: Laxman Dewangan <ldewangan@nvidia.com>
    Acked-by: Noah Wager <noah.wager@canonical.com>
    Acked-by: Jacob Martin <jacob.martin@canonical.com>
    Signed-off-by: Noah Wager <noah.wager@canonical.com>

--- drivers/phy/tegra/xusb-tegra186.c
+++ drivers/phy/tegra/xusb-tegra186.c
@@ -656,6 +656,12 @@ static void tegra186_utmi_bias_pad_power_on(struct tegra_xusb_padctl *padctl)
 		value &= ~CYA_TRK_CODE_UPDATE_ON_IDLE;
 		padctl_writel(padctl, value, XUSB_PADCTL_USB2_BIAS_PAD_CTL2);
 	} else {
+		/* Disable periodic tracking explicitly before disabling
+		 * the clock to avoid unstable connections
+		 */
+		value = padctl_readl(padctl, XUSB_PADCTL_USB2_BIAS_PAD_CTL2);
+		value &= ~(USB2_TRK_HW_MODE | CYA_TRK_CODE_UPDATE_ON_IDLE);
+		padctl_writel(padctl, value, XUSB_PADCTL_USB2_BIAS_PAD_CTL2);
 		clk_disable_unprepare(priv->usb2_trk_clk);
 	}
 
@@ -1703,7 +1709,6 @@ const struct tegra_xusb_padctl_soc tegra234_xusb_padctl_soc = {
 	.num_supplies = ARRAY_SIZE(tegra194_xusb_padctl_supply_names),
 	.supports_gen2 = true,
 	.poll_trk_completed = true,
-	.trk_hw_mode = true,
 	.supports_lp_cfg_en = true,
 };
 EXPORT_SYMBOL_GPL(tegra234_xusb_padctl_soc);
