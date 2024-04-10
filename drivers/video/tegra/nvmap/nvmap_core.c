b60fded6ad91b265c77a4f07bc3ba856aa00c4ef
N V S Abhishek <nabhishek@nvidia.com>
2024-04-10 12:29:11 +0000

    nvmap: Fix type conversion sparse warning
    Fix sparse warning of incompatible type conversion in nvmap_core. Add __force type conversion to void* as a fix.
    
    Bug 4513982
    
    Change-Id: Ia7d86a9751a71decda2f3be3f5204c3cd2fd006e
    Signed-off-by: N V S Abhishek <nabhishek@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/c/linux-nv-oot/+/3112749
    Reviewed-by: Ketan Patil <ketanp@nvidia.com>
    Reviewed-by: Sachin Nikam <snikam@nvidia.com>
    GVS: Gerrit_Virtual_Submit <buildbot_gerritrpt@nvidia.com>
    Reviewed-by: Ashish Mhetre <amhetre@nvidia.com>

--- drivers/video/tegra/nvmap/nvmap_core.c
+++ drivers/video/tegra/nvmap/nvmap_core.c
@@ -107,7 +107,7 @@ void *__nvmap_mmap(struct nvmap_handle *h)
 		vaddr = vmap(pages, nr_pages, VM_MAP, prot);
 	} else {
 #if defined(CONFIG_GENERIC_IOREMAP)
-		vaddr = ioremap_prot(h->carveout->base, adj_size, pgprot_val(prot));
+		vaddr = (__force void *)ioremap_prot(h->carveout->base, adj_size, pgprot_val(prot));
 #else
 		vaddr = (__force void *)__ioremap(h->carveout->base, adj_size,
 			 prot);
