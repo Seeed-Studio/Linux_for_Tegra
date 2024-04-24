05d25c7dcdd5209575f28a2ec9db253ca06f92a4
Hyong Bin Kim <hyongbink@nvidia.com>
2024-04-24 11:51:15 +0900

    nv-public: dts: Add missing kernel include file
    Add missing include file for automotive DTB.
    
    Bug 4355701
    
    Change-Id: I26436ec9b6c39283752bb9f7ba1ac31c11847c54
    Signed-off-by: Hyong Bin Kim <hyongbink@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/c/device/hardware/nvidia/t23x-public-dts/+/3122877
    GVS: Gerrit_Virtual_Submit <buildbot_gerritrpt@nvidia.com>
    Reviewed-by: Sumeet Gupta <sumeetg@nvidia.com>
    Reviewed-by: svcacv <svcacv@nvidia.com>
    Reviewed-by: Suresh Venkatachalam <skathirampat@nvidia.com>

---
+++ include/nvidia-oot/dt-bindings/soc/ga10b-fuse.h
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: GPL-2.0
+// SPDX-FileCopyrightText: Copyright (c) 2021-2024, NVIDIA CORPORATION & AFFILIATES.  All rights reserved.
+
+
+#ifndef _DT_BINDINGS_GA10B_FUSE_H
+#define _DT_BINDINGS_GA10B_FUSE_H
+
+#define GA10B_FUSE_OPT_GPC_DISABLE	0x288
+#define GA10B_FUSE_OPT_FBP_DISABLE	0xb70
+#define GA10B_FUSE_OPT_TPC_DISABLE	0x30c
+
+#endif
