044a70ac6241b0cd5a4da98cb697499a7a4e2a03
Jon Hunter <jonathanh@nvidia.com>
2024-05-20 20:31:28 +0100

    drivers: Fix dummy module license
    The correct module license string for drivers is "GPL" and not "GPL
    V2". Note that "GPL" in the module license indicates GPLv2 according to
    the kernel documentation. Using "GPL V2" causes the following kernel
    warns to be printed when the module is loaded ...
    
     r8168: module license 'GPL V2' taints kernel.
     Disabling lock debugging due to kernel taint
     r8168: module license taints kernel.
    
    Fix this by correcting the license string for dummy drivers.
    
    Bug 4606591
    Bug 4471899
    
    Change-Id: Ib785b2d3c90234e279cb1d804bf9a9842e6e5084
    Signed-off-by: Jon Hunter <jonathanh@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/c/linux-nv-oot/+/3141628
    GVS: buildbot_gerritrpt <buildbot_gerritrpt@nvidia.com>

--- drivers/net/ethernet/realtek/r8168/r8168_dummy.c
+++ drivers/net/ethernet/realtek/r8168/r8168_dummy.c
@@ -1,7 +1,5 @@
-// SPDX-License-Identifier: GPL-2.0
-/*
- * Copyright (c) 2024, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
- */
+// SPDX-License-Identifier: GPL-2.0-only
+// SPDX-FileCopyrightText: Copyright (c) 2024 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 
 #include <linux/module.h>
 
@@ -14,4 +12,4 @@ device_initcall(r8168_dummy_dummy_init);
 
 MODULE_AUTHOR("Laxman Dewangan <ldewangan@nvidia.com>");
 MODULE_DESCRIPTION("Dummy R8168 dummy driver");
-MODULE_LICENSE("GPL V2");
+MODULE_LICENSE("GPL");
