26ecf5efbeca36327c91feae2d652238b17b8eda
gokull <gokull@nvidia.com>
2024-05-24 11:49:31 +0000

    [T264/dt_bindings] ufs stream id
    Create a sequence of stream id for
    ufs in generic DT folder structure.
    
    Duplicate TEGRA_UFS_STREAM_ID as
    TEGRA264_UFS_STREAM_ID.
    
    Jira ESDP-24833
    
    Change-Id: I03717cf92833f00cdc7d371eaf56e8293d5ea81d
    Reviewed-on: https://git-master.nvidia.com/r/c/device/hardware/nvidia/t264-public-dts/+/3145995
    Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>
    Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>

---
+++ include/kernel-t264/dt-bindings/memory/tegra264-smmu-streamid-ufs.h
@@ -0,0 +1,50 @@
+/*
+*SPDX-FileCopyrightText: Copyright (c) 2024 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+*SPDX-License-Identifier: LicenseRef-NvidiaProprietary
+*
+*NVIDIA CORPORATION, its affiliates and licensors retain all intellectual
+*property and proprietary rights in and to this material, related
+*documentation and any modifications thereto. Any use, reproduction,
+*disclosure or distribution of this material and related documentation
+*without an express license agreement from NVIDIA CORPORATION or
+*its affiliates is strictly prohibited.
+*/
+
+#ifndef __TEGRA264_SMMU_STREAMID_UFS_H__
+#define __TEGRA264_SMMU_STREAMID_UFS_H__
+
+#include "tegra264-mc.h"
+
+#define TEGRA264_SID_UFS_1  (TEGRA264_SID_UFS + 0x01)
+#define TEGRA264_SID_UFS_2  (TEGRA264_SID_UFS + 0x02)
+#define TEGRA264_SID_UFS_3  (TEGRA264_SID_UFS + 0x03)
+#define TEGRA264_SID_UFS_4  (TEGRA264_SID_UFS + 0x04)
+#define TEGRA264_SID_UFS_5  (TEGRA264_SID_UFS + 0x05)
+#define TEGRA264_SID_UFS_6  (TEGRA264_SID_UFS + 0x06)
+#define TEGRA264_SID_UFS_7  (TEGRA264_SID_UFS + 0x07)
+#define TEGRA264_SID_UFS_8  (TEGRA264_SID_UFS + 0x08)
+#define TEGRA264_SID_UFS_9 (TEGRA264_SID_UFS + 0x09)
+#define TEGRA264_SID_UFS_10 (TEGRA264_SID_UFS + 0x0a)
+#define TEGRA264_SID_UFS_11 (TEGRA264_SID_UFS + 0x0b)
+#define TEGRA264_SID_UFS_12 (TEGRA264_SID_UFS + 0x0c)
+#define TEGRA264_SID_UFS_13 (TEGRA264_SID_UFS + 0x0d)
+#define TEGRA264_SID_UFS_14 (TEGRA264_SID_UFS + 0x0e)
+#define TEGRA264_SID_UFS_15 (TEGRA264_SID_UFS + 0x0f)
+#define TEGRA264_SID_UFS_16 (TEGRA264_SID_UFS + 0x10)
+#define TEGRA264_SID_UFS_17 (TEGRA264_SID_UFS + 0x11)
+#define TEGRA264_SID_UFS_18 (TEGRA264_SID_UFS + 0x12)
+#define TEGRA264_SID_UFS_19 (TEGRA264_SID_UFS + 0x13)
+#define TEGRA264_SID_UFS_20 (TEGRA264_SID_UFS + 0x14)
+#define TEGRA264_SID_UFS_21 (TEGRA264_SID_UFS + 0x15)
+#define TEGRA264_SID_UFS_22 (TEGRA264_SID_UFS + 0x16)
+#define TEGRA264_SID_UFS_23 (TEGRA264_SID_UFS + 0x17)
+#define TEGRA264_SID_UFS_24 (TEGRA264_SID_UFS + 0x18)
+#define TEGRA264_SID_UFS_25 (TEGRA264_SID_UFS + 0x19)
+#define TEGRA264_SID_UFS_26 (TEGRA264_SID_UFS + 0x1a)
+#define TEGRA264_SID_UFS_27 (TEGRA264_SID_UFS + 0x1b)
+#define TEGRA264_SID_UFS_28 (TEGRA264_SID_UFS + 0x1c)
+#define TEGRA264_SID_UFS_29 (TEGRA264_SID_UFS + 0x1d)
+#define TEGRA264_SID_UFS_30 (TEGRA264_SID_UFS + 0x1e)
+#define TEGRA264_SID_UFS_31 (TEGRA264_SID_UFS + 0x1f)
+
+#endif /* __TEGRA264_SMMU_STREAMID_UFS_H__ */
