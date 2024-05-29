955de38d701862f83d722c2164d31aafce19dfbf
Jon Hunter <jonathanh@nvidia.com>
2024-05-29 10:16:05 +0100

    net: nvethernet: Fix support for Linux v6.9
    When nvethernet was updated to support Linux v6.9 kernels, the code that
    checks if the variable 'eee_req->advertised' is zero or non-zero was not
    updated correctly. For Linux v6.9, the variable 'eee_req->advertised' is
    a bitmask and so cannot be checked directly to see if it is zero or
    non-zero. Building the nvethernet driver with the flag '-Werror=address'
    exposed this issue. Fix this by using the 'linkmode_empty()' function to
    determine if 'eee_req->advertised' is zero or non-zero for Linux v6.9
    kernels.
    
    Bug 4471899
    Bug 4662166
    
    Change-Id: Id4080d62006226648cd398dc8652578c74dd8158
    Signed-off-by: Jon Hunter <jonathanh@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/c/linux-nv-oot/+/3158064
    Tested-by: Shanker Donthineni <sdonthineni@nvidia.com>
    Reviewed-by: Rohit Khanna <rokhanna@nvidia.com>
    Reviewed-by: svcacv <svcacv@nvidia.com>
    Reviewed-by: Shanker Donthineni <sdonthineni@nvidia.com>
    GVS: buildbot_gerritrpt <buildbot_gerritrpt@nvidia.com>
    Reviewed-by: Bitan Biswas <bbiswas@nvidia.com>
    Tested-by: Rohit Khanna <rokhanna@nvidia.com>

--- drivers/net/ethernet/nvidia/nvethernet/ethtool.c
+++ drivers/net/ethernet/nvidia/nvethernet/ethtool.c
@@ -1192,7 +1192,11 @@ static inline void validate_eee_conf(struct net_device *ndev,
 	 * on whether EEE has toggled or not.
 	 */
 	if (!eee_req->eee_enabled && !eee_req->tx_lpi_enabled &&
+#if defined(NV_ETHTOOL_KEEE_STRUCT_PRESENT) /* Linux v6.9 */
+	    !linkmode_empty(eee_req->advertised)) {
+#else
 	    eee_req->advertised) {
+#endif
 		if (eee_req->eee_enabled != cur_eee.eee_enabled) {
 			netdev_warn(ndev, "EEE off. Set Rx LPI off\n");
 #if defined(NV_ETHTOOL_KEEE_STRUCT_PRESENT) /* Linux v6.9 */
@@ -1207,7 +1211,11 @@ static inline void validate_eee_conf(struct net_device *ndev,
 	}
 
 	if (!eee_req->eee_enabled && eee_req->tx_lpi_enabled &&
+#if defined(NV_ETHTOOL_KEEE_STRUCT_PRESENT) /* Linux v6.9 */
+	    linkmode_empty(eee_req->advertised)) {
+#else
 	    !eee_req->advertised) {
+#endif
 		if (eee_req->eee_enabled != cur_eee.eee_enabled) {
 			netdev_warn(ndev, "EEE off. Set Tx LPI off\n");
 			eee_req->tx_lpi_enabled = OSI_DISABLE;
@@ -1227,7 +1235,11 @@ static inline void validate_eee_conf(struct net_device *ndev,
 	}
 
 	if (!eee_req->eee_enabled && eee_req->tx_lpi_enabled &&
+#if defined(NV_ETHTOOL_KEEE_STRUCT_PRESENT) /* Linux v6.9 */
+	    !linkmode_empty(eee_req->advertised)) {
+#else
 	    eee_req->advertised) {
+#endif
 		if (eee_req->eee_enabled != cur_eee.eee_enabled) {
 			netdev_warn(ndev, "EEE off. Set Tx & Rx LPI off\n");
 			eee_req->tx_lpi_enabled = OSI_DISABLE;
@@ -1243,7 +1255,11 @@ static inline void validate_eee_conf(struct net_device *ndev,
 	}
 
 	if (eee_req->eee_enabled && !eee_req->tx_lpi_enabled &&
+#if defined(NV_ETHTOOL_KEEE_STRUCT_PRESENT) /* Linux v6.9 */
+	    linkmode_empty(eee_req->advertised)) {
+#else
 	    !eee_req->advertised) {
+#endif
 		if (eee_req->eee_enabled != cur_eee.eee_enabled) {
 			netdev_warn(ndev, "EEE on. Set Tx & Rx LPI on\n");
 			eee_req->tx_lpi_enabled = OSI_ENABLE;
