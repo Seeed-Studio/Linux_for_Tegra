776ab71140876b8500e02e13dc6d5a63d22d2982
Santosh BS <santoshb@nvidia.com>
2024-06-02 18:53:16 +0000

    gpu: host1x-nvhost: fetch host1x data based on instance
    Get the right host1x data pointer according to the instance passed.
    
    Bug 4505161
    Jira NVGPU-11046
    
    Change-Id: I2bf671236a2fb3a89c87c22d2d7b5f81cf035524
    Signed-off-by: Santosh BS <santoshb@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/c/linux-nv-oot/+/3149447
    Reviewed-by: Mikko Perttunen <mperttunen@nvidia.com>
    Tested-by: Seema Khowala <seemaj@nvidia.com>
    GVS: buildbot_gerritrpt <buildbot_gerritrpt@nvidia.com>

--- drivers/gpu/host1x-nvhost/nvhost.c
+++ drivers/gpu/host1x-nvhost/nvhost.c
@@ -81,20 +81,46 @@ struct platform_device *nvhost_get_default_device(void)
 }
 EXPORT_SYMBOL(nvhost_get_default_device);
 
-struct host1x *nvhost_get_host1x(struct platform_device *pdev)
+static struct platform_device *nvhost_get_host1x_device(int instance)
+{
+	struct platform_device *host1x_pdev;
+	struct device_node *np;
+	int numa_node;
+
+	for_each_matching_node(np, host1x_match) {
+		host1x_pdev = of_find_device_by_node(np);
+		if (host1x_pdev) {
+			numa_node = dev_to_node(&host1x_pdev->dev);
+			if (numa_node == NUMA_NO_NODE || numa_node == instance)
+				return host1x_pdev;
+		}
+	}
+
+	return NULL;
+}
+
+struct host1x *nvhost_get_host1x(struct platform_device *client_pdev)
 {
 	struct platform_device *host1x_pdev;
 	struct host1x *host1x;
+	int numa_node = NUMA_NO_NODE;
 
-	host1x_pdev = nvhost_get_default_device();
+	if (client_pdev)
+		numa_node = dev_to_node(&client_pdev->dev);
+
+	if (numa_node == NUMA_NO_NODE)
+		host1x_pdev = nvhost_get_default_device();
+	else
+		host1x_pdev = nvhost_get_host1x_device(numa_node);
+
 	if (!host1x_pdev) {
-		dev_dbg(&pdev->dev, "host1x device not available\n");
+		dev_dbg(&client_pdev->dev, "host1x device not available\n");
 		return NULL;
 	}
 
 	host1x = platform_get_drvdata(host1x_pdev);
 	if (!host1x) {
-		dev_warn(&pdev->dev, "No platform data for host1x!\n");
+		dev_warn(&client_pdev->dev, "No platform data for host1x!\n");
 		return NULL;
 	}
 
