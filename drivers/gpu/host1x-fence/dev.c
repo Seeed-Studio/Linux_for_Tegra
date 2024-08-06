783f728b327f32a3bb2074e758105219de1bf6ab
jianjunm <jianjunm@nvidia.com>
2024-08-06 10:28:52 +0800

    Restore "host1x-fence:Free the kzalloc mem"
    Restore the memory leak fix that was reverted
    due to it caused[k6.8/l4t/]ap_compute_level1_test
    to fail in GVS.
    
    New fix is intended to ensure that the fence
    and callback cannot be referenced after release.
    
    This reverts commit 42645ad0491eee4529c96ba8a081d2d2a3ddbe46.
    
    Bug 4779016
    
    Change-Id: Ie2845e1830bb00cb49e7a3c03a91129b28ca62e9
    Signed-off-by: jianjunm <jianjunm@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/c/linux-nv-oot/+/3188619
    Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>
    Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>

--- drivers/gpu/host1x-fence/dev.c
+++ drivers/gpu/host1x-fence/dev.c
@@ -1,9 +1,5 @@
 // SPDX-License-Identifier: GPL-2.0-only
-/*
- * Host1x fence UAPI
- *
- * Copyright (c) 2022-2024, NVIDIA CORPORATION.  All rights reserved.
- */
+// SPDX-FileCopyrightText: Copyright (c) 2022-2024, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 
 #include <nvidia/conftest.h>
 
@@ -205,11 +201,11 @@ struct host1x_pollfd {
 static int host1x_pollfd_release(struct inode *inode, struct file *file)
 {
 	struct host1x_pollfd *pollfd = file->private_data;
-	struct host1x_pollfd_fence *pfd_fence;
+	struct host1x_pollfd_fence *pfd_fence, *pfd_fence_temp;
 
 	mutex_lock(&pollfd->lock);
 
-	list_for_each_entry(pfd_fence, &pollfd->fences, list) {
+	list_for_each_entry_safe(pfd_fence, pfd_fence_temp, &pollfd->fences, list) {
 		if (pfd_fence->callback_set) {
 			if (dma_fence_remove_callback(pfd_fence->fence, &pfd_fence->callback))
 				host1x_fence_cancel(pfd_fence->fence);
@@ -220,9 +216,12 @@ static int host1x_pollfd_release(struct inode *inode, struct file *file)
 		spin_unlock(pfd_fence->fence->lock);
 
 		dma_fence_put(pfd_fence->fence);
+		kfree(pfd_fence);
 	}
 
 	mutex_unlock(&pollfd->lock);
+
+	kfree(pollfd);
 
 	return 0;
 }
@@ -253,6 +252,7 @@ static unsigned int host1x_pollfd_poll(struct file *file, poll_table *wait)
 
 			dma_fence_put(pfd_fence->fence);
 			list_del(&pfd_fence->list);
+			kfree(pfd_fence);
 		}
 	}
 
