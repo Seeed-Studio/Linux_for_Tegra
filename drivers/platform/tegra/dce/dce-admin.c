7f0ed24743c5758b6ed122e9162b6bd53188c073
anupamg <anupamg@nvidia.com>
2024-10-07 21:04:09 +0000

    DCE-KMD: Add check for params arg in dce_admin...
    - Add check for params arg in dce_admin_handle_ipc_received_event().
    
    - dce_admin_handle_ipc_received_event() is a one of the FSM event
      functions which shares common FSM event function prototype:
        int (*fsm_event_handle)(struct tegra_dce *d, void *params);
    
    - But dce_admin_handle_ipc_received_event() implementation doesn't
      need 'params' argument and therefore callers are expected to
      pass NULL.
    
    - This change will add a check inside this function to check
      if the params are NULL and print a warning message otherwise.
    
    JIRA TDS-16126
    
    Change-Id: I70b73d195d866b7b6bb43b5430dccfb0bc3cb486
    Signed-off-by: anupamg <anupamg@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/c/linux-nv-oot/+/3225248
    GVS: buildbot_gerritrpt <buildbot_gerritrpt@nvidia.com>
    Reviewed-by: Arun Swain <arswain@nvidia.com>
    Reviewed-by: svcacv <svcacv@nvidia.com>
    Reviewed-by: Mahesh Kumar <mahkumar@nvidia.com>

--- drivers/platform/tegra/dce/dce-admin.c
+++ drivers/platform/tegra/dce/dce-admin.c
@@ -337,7 +337,9 @@ out:
 
 int dce_admin_handle_ipc_received_event(struct tegra_dce *d, void *params)
 {
-	USE(params);
+	if (params != NULL)
+		dce_warn(d, "Params aren't expected in this function\n");
+
 	dce_wakeup_interruptible(d, DCE_WAIT_ADMIN_IPC);
 	return 0;
 }
