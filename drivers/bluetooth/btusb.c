3897c0efcc7e8d5dd149d185f1055dedf9240a6e
Shobek Attupurath <sattupurath@nvidia.com>
2024-11-05 02:24:38 +0000

    NVIDIA: SAUCE: bt_usb: Disable RTL8852CE upstream driver
    Disable RTL8852CE upstream bluetooth driver detection to
    support downstream driver
    
    http://nvbugs/4915378
    
    Signed-off-by: Shobek Attupurath <sattupurath@nvidia.com>
    Reviewed-by: Ashutosh Jha <ajha@nvidia.com>
    Signed-off-by: Vishwaroop A <va@nvidia.com>
    Acked-by: Noah Wager <noah.wager@canonical.com>
    Acked-by: Jacob Martin <jacob.martin@canonical.com>
    Signed-off-by: Noah Wager <noah.wager@canonical.com>

--- drivers/bluetooth/btusb.c
+++ drivers/bluetooth/btusb.c
@@ -536,8 +536,7 @@ static const struct usb_device_id quirks_table[] = {
 						     BTUSB_WIDEBAND_SPEECH },
 	{ USB_DEVICE(0x13d3, 0x3587), .driver_info = BTUSB_REALTEK |
 						     BTUSB_WIDEBAND_SPEECH },
-	{ USB_DEVICE(0x13d3, 0x3586), .driver_info = BTUSB_REALTEK |
-						     BTUSB_WIDEBAND_SPEECH },
+	{ USB_DEVICE(0x13d3, 0x3586), .driver_info = BTUSB_IGNORE },
 	{ USB_DEVICE(0x13d3, 0x3592), .driver_info = BTUSB_REALTEK |
 						     BTUSB_WIDEBAND_SPEECH },
 	{ USB_DEVICE(0x0489, 0xe122), .driver_info = BTUSB_REALTEK |
