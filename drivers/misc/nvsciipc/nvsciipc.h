936e6687ef3e6d19e5fc98e3ffec3c8c314559e3
Suneel Kumar Pemmineti <spemmineti@nvidia.com>
2024-11-19 10:53:54 +0000

    nvsciipc: Include change for nvsciipc_ioctl.h
    - KMD to refer nvsciipc_ioctl.h from
    $TEGRA_TOP/gpu/drv/drivers/nvsci/nvsciipc/inc-linux
    
    Jira: NVIPC-2817
    Change-Id: Ic8d86c1320759bf9260d5bb9b4ee82441b0bbb71
    Signed-off-by: Suneel Kumar Pemmineti <spemmineti@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/c/linux-nv-oot/+/3252263
    Reviewed-by: Simon Je <sje@nvidia.com>
    GVS: buildbot_gerritrpt <buildbot_gerritrpt@nvidia.com>
    Reviewed-by: svcacv <svcacv@nvidia.com>

--- drivers/misc/nvsciipc/nvsciipc.h
+++ drivers/misc/nvsciipc/nvsciipc.h
@@ -1,6 +1,7 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 /*
- * Copyright (c) 2019-2023, NVIDIA CORPORATION. All rights reserved.
+ * SPDX-FileCopyrightText: Copyright (c) 2019-2024, NVIDIA CORPORATION & AFFILIATES.
+ * All rights reserved.
  */
 
 #ifndef __NVSCIIPC_KERNEL_H__
@@ -8,7 +9,7 @@
 
 #include <linux/nvscierror.h>
 #include <linux/nvsciipc_interface.h>
-#include <uapi/linux/nvsciipc_ioctl.h>
+#include "nvsciipc_ioctl.h"
 
 #define ERR(...) pr_err("nvsciipc: " __VA_ARGS__)
 #define INFO(...) pr_info("nvsciipc: " __VA_ARGS__)
