7d8eddb2b83b94001b37b297f5b66924ce23b6e9
bigbearishappy <953308023@qq.com>
2024-12-04 07:41:04 +0000

    add support of recomputer j401 with orin nx 8g

--- source/do_copy.sh
+++ source/do_copy.sh
@@ -2,4 +2,6 @@
 
 cp ./kernel_out/kernel-devicetree/generic-dts/dtbs/tegra234-j401-p3768-0000+p3767-0001-recomputer.dtb ../kernel/dtb/
 
+cp ./kernel_out/kernel-devicetree/generic-dts/dtbs/tegra234-p3767-camera-p3768-imx219-dual-seeed.dtbo ../kernel/dtb/
+
 cp  ./kernel_out/kernel/kernel-jammy-src/arch/arm64/boot/Image ../kernel/Image
