eae40c03692aedd5cfe0c92a799101dd0f21e68a
bigbearishappy <953308023@qq.com>
2024-12-04 09:17:18 +0000

    upload dtb files for recomputer j401 with orin nx 8g and orin nano 4g/8g

--- source/do_copy.sh
+++ source/do_copy.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+cp ./kernel_out/kernel-devicetree/generic-dts/dtbs/tegra234-j401-p3768-0000+p3767-0000-recomputer.dtb ../kernel/dtb/
 cp ./kernel_out/kernel-devicetree/generic-dts/dtbs/tegra234-j401-p3768-0000+p3767-0001-recomputer.dtb ../kernel/dtb/
+cp ./kernel_out/kernel-devicetree/generic-dts/dtbs/tegra234-j401-p3768-0000+p3767-0003-recomputer.dtb ../kernel/dtb/
+cp ./kernel_out/kernel-devicetree/generic-dts/dtbs/tegra234-j401-p3768-0000+p3767-0004-recomputer.dtb ../kernel/dtb/
 
 cp ./kernel_out/kernel-devicetree/generic-dts/dtbs/tegra234-p3767-camera-p3768-imx219-dual-seeed.dtbo ../kernel/dtb/
 
