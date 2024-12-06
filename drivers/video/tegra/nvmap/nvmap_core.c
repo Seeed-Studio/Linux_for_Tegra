8a948abd7bd0b4a896f133f6152d216a48d8454a
Surbhi Singh <surbhis@nvidia.com>
2024-12-06 09:36:34 +0000

    video: tegra: nvmap: Fix INT30-C overflow defects
    Adding check for overflow when two unsigned integers are added and return error in case of overflow condition.
    
    JIRA: TMM-5724
    Bug 4479044
    
    Change-Id: I8c66af46a68b64502fbfe754aa973bf068342003
    Signed-off-by: Surbhi Singh <surbhis@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/c/linux-nv-oot/+/3263286
    GVS: buildbot_gerritrpt <buildbot_gerritrpt@nvidia.com>
    Reviewed-by: Ketan Patil <ketanp@nvidia.com>
    Reviewed-by: Sachin Nikam <snikam@nvidia.com>

--- drivers/video/tegra/nvmap/nvmap_core.c
+++ drivers/video/tegra/nvmap/nvmap_core.c
@@ -44,7 +44,7 @@ void *__nvmap_mmap(struct nvmap_handle *h)
 {
 	pgprot_t prot;
 	void *vaddr;
-	unsigned long adj_size;
+	unsigned long adj_size, sum;
 	struct page **pages = NULL;
 	int i = 0;
 
@@ -96,7 +96,10 @@ void *__nvmap_mmap(struct nvmap_handle *h)
 
 	/* carveout - explicitly map the pfns into a vmalloc area */
 	adj_size = nvmap_get_heap_block_base(h->carveout) & ~PAGE_MASK;
-	adj_size += h->size;
+	if (check_add_overflow(adj_size, h->size, &sum))
+		goto dec_kmaps;
+
+	adj_size = sum;
 	adj_size = PAGE_ALIGN(adj_size);
 
 	if (pfn_valid(__phys_to_pfn(nvmap_get_heap_block_base(h->carveout) & PAGE_MASK))) {
@@ -149,6 +152,7 @@ void *__nvmap_mmap(struct nvmap_handle *h)
 out:
 	if (pages)
 		vfree(pages);
+dec_kmaps:
 	nvmap_kmaps_dec(h);
 put_handle:
 	nvmap_handle_put(h);
