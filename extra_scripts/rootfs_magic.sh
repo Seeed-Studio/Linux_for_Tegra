b0ee37330621d21848edc5b887edae4fe336497d
jian xiong <953308023@qq.com>
2024-12-21 11:41:42 +0000

    update rootfs_magic.sh
    Signed-off-by: jian xiong <953308023@qq.com>

--- extra_scripts/rootfs_magic.sh
+++ extra_scripts/rootfs_magic.sh
@@ -1,8 +1,11 @@
  #!/bin/bash
+ set -x
+
 echo 'Acquire::http::Proxy "http://192.168.1.77:3142";' >> /etc/apt/apt.conf
 
 apt-get update
-apt install -y nvidia-jetpack pulseaudio-module-bluetooth
+apt install -y nvidia-jetpack 
+apt install -y pulseaudio-module-bluetooth
 systemctl disable nvgetty.service
 apt-get clean
 rm /etc/apt/apt.conf
