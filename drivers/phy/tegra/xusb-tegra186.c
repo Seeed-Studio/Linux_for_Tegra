bc48269a662972f0b5102d30aa056c7ab49244fd
haotienh <haotienh@nvidia.com>
2025-02-06 21:31:19 +0000

    NVIDIA: SAUCE: phy: tegra: xusb: Fix setting WAKE_WALK_EN
    Set WAKE_WALK_EN correctly, after reviewing the programming sequences.
    
    http://nvbugs/4804339
    
    Signed-off-by: haotienh <haotienh@nvidia.com>
    Reviewed-by: Henry Lin <henryl@nvidia.com>
    Reviewed-by: EJ Hsu <ejh@nvidia.com>
    Reviewed-by: WK Tsai <wtsai@nvidia.com>
    Reviewed-by: Wayne Chang <waynec@nvidia.com>
    Signed-off-by: Vishwaroop A <va@nvidia.com>
    Acked-by: Noah Wager <noah.wager@canonical.com>
    Acked-by: Jacob Martin <jacob.martin@canonical.com>
    Signed-off-by: Noah Wager <noah.wager@canonical.com>

--- drivers/phy/tegra/xusb-tegra186.c
+++ drivers/phy/tegra/xusb-tegra186.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: GPL-2.0
 /*
- * SPDX-FileCopyrightText: Copyright (c) 2016-2024, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+ * SPDX-FileCopyrightText: Copyright (c) 2016-2025, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
  */
 
 #include <linux/delay.h>
@@ -428,8 +428,7 @@ static int tegra186_utmi_enable_phy_sleepwalk(struct tegra_xusb_lane *lane,
 
 	/* enable the trigger of the sleepwalk logic */
 	value = ao_readl(priv, XUSB_AO_UTMIP_SLEEPWALK_CFG(index));
-	value |= LINEVAL_WALK_EN;
-	value &= ~WAKE_WALK_EN;
+	value |= LINEVAL_WALK_EN | WAKE_WALK_EN;
 	ao_writel(priv, value, XUSB_AO_UTMIP_SLEEPWALK_CFG(index));
 
 	/* reset the walk pointer and clear the alarm of the sleepwalk logic,
