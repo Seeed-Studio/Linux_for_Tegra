f16bcf63824b2bd5b9225ff7cf05aaa211f9bace
jian xiong <953308023@qq.com>
2025-02-08 07:57:28 +0000

    upload CI and relative files

---
+++ extra_scripts/rootfs_magic.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -x
+ 
+echo 'Acquire::http::Proxy "http://192.168.1.77:3142";' >> /etc/apt/apt.conf
+
+apt-get update
+apt install -y nvidia-jetpack
+apt install -y pulseaudio-module-bluetooth
+systemctl disable nvgetty.service
+apt-get clean
+rm /etc/apt/apt.conf
+
