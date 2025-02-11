99875ea0c673affcc5cd20d00134f61c3867b68e
jian xiong <953308023@qq.com>
2025-02-11 10:59:46 +0000

    upload config file for j40mini and it's relative file

--- source/do_copy.sh
+++ source/do_copy.sh
@@ -15,6 +15,11 @@ cp ./kernel_out/kernel-devicetree/generic-dts/dtbs/tegra234-j201-p3768-0000+p376
 cp ./kernel_out/kernel-devicetree/generic-dts/dtbs/tegra234-j201-p3768-0000+p3767-0003-recomputer-indu.dtb ../kernel/dtb/
 cp ./kernel_out/kernel-devicetree/generic-dts/dtbs/tegra234-j201-p3768-0000+p3767-0004-recomputer-indu.dtb ../kernel/dtb/
 
+cp ./kernel_out/kernel-devicetree/generic-dts/dtbs/tegra234-j40mini-p3768-0000+p3767-0000-recomputer.dtb ../kernel/dtb/
+cp ./kernel_out/kernel-devicetree/generic-dts/dtbs/tegra234-j40mini-p3768-0000+p3767-0001-recomputer.dtb ../kernel/dtb/
+cp ./kernel_out/kernel-devicetree/generic-dts/dtbs/tegra234-j40mini-p3768-0000+p3767-0003-recomputer.dtb ../kernel/dtb/
+cp ./kernel_out/kernel-devicetree/generic-dts/dtbs/tegra234-j40mini-p3768-0000+p3767-0004-recomputer.dtb ../kernel/dtb/
+
 cp ./kernel_out/kernel-devicetree/generic-dts/dtbs/tegra234-p3767-camera-p3768-imx219-dual-seeed.dtbo ../kernel/dtb/
 
 cp  ./kernel_out/kernel/kernel-jammy-src/arch/arm64/boot/Image ../kernel/Image
