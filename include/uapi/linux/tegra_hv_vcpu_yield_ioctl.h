f204e3ae777bbe5cc188a316caf9e5976cb21c0a
Manish Bhardwaj <mbhardwaj@nvidia.com>
2025-02-12 17:22:26 +0000

    nvidia-oot: vcpu yield driver cert & misra fixes
    using this patch fixing below cert & misra errors:-
    - cert_dcl37_c_violation: The reserved identifier
      "_UAPI_TEGRA_HV_VCPU_YIELD_IOCTL_H_", which is reserved for use as
      identifiers with file scope in both the ordinary and tag name spaces,
      is defined.
    - misra_c_2012_rule_21_1_violation: Defining or undefining a reserved
      name "_UAPI_TEGRA_HV_VCPU_YIELD_IOCTL_H_", which is an identifier or
      macro name beginning with an underscore.
    - misra_c_2012_rule_21_2_violation: "_UAPI_TEGRA_HV_VCPU_YIELD_IOCTL_H_",
      an identifier or macro name beginning with an underscore, shall not be
      declared.
    
    JIRA ESLC-8378
    
    Change-Id: I1d8d5f437793a0b964b1ed195f70ab84c6104895
    Signed-off-by: Manish Bhardwaj <mbhardwaj@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/c/linux-nv-oot/+/3301267
    Reviewed-by: Sumeet Gupta <sumeetg@nvidia.com>
    Reviewed-by: Suresh Venkatachalam <skathirampat@nvidia.com>
    GVS: buildbot_gerritrpt <buildbot_gerritrpt@nvidia.com>

--- include/uapi/linux/tegra_hv_vcpu_yield_ioctl.h
+++ include/uapi/linux/tegra_hv_vcpu_yield_ioctl.h
@@ -1,10 +1,10 @@
 /* SPDX-License-Identifier: (GPL-2.0 WITH Linux-syscall-note)
  *
- * Copyright (c) 2022-2023, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+ * Copyright (c) 2022-2025, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
  */
 
-#ifndef _UAPI_TEGRA_HV_VCPU_YIELD_IOCTL_H_
-#define _UAPI_TEGRA_HV_VCPU_YIELD_IOCTL_H_
+#ifndef TEGRA_HV_VCPU_YIELD_IOCTL_H
+#define TEGRA_HV_VCPU_YIELD_IOCTL_H
 
 #include <linux/ioctl.h>
 
@@ -20,4 +20,4 @@ struct vcpu_yield_start_ctl {
 #define VCPU_YIELD_START_IOCTL _IOW(VCPU_YIELD_IOC_MAGIC, \
 		VCPU_YIELD_START_CMDID, struct vcpu_yield_start_ctl)
 
-#endif /* #ifndef _UAPI_TEGRA_HV_VCPU_YIELD_IOCTL_H_ */
+#endif /* #ifndef TEGRA_HV_VCPU_YIELD_IOCTL_H */
