0440a552b50e6c56cd4c3394e5dffd318a02b583
Yijun Zhou <yijunz@nvidia.com>
2025-02-18 08:25:01 +0000

    t23x: nv-public: Correct license for Android
    Device-tree code should use GPL license.
    
    Bug 5095651
    
    Change-Id: Id9c82f3c1b48dafe34d2ff2b1ef8edaff7728530
    Signed-off-by: Yijun Zhou <yijunz@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/c/device/hardware/nvidia/t23x-public-dts/+/3305055
    Reviewed-by: Jon Hunter <jonathanh@nvidia.com>
    GVS: buildbot_gerritrpt <buildbot_gerritrpt@nvidia.com>
    Reviewed-by: Brad Griffis <bgriffis@nvidia.com>

--- include/platforms/dt-bindings/tegra234-android-bootargs.h
+++ include/platforms/dt-bindings/tegra234-android-bootargs.h
@@ -1,13 +1,5 @@
-/*
- * SPDX-FileCopyrightText: Copyright (c) 2024, NVIDIA CORPORATION.  All rights reserved.
- * SPDX-License-Identifier: LicenseRef-NvidiaProprietary
- *
- * NVIDIA Corporation and its licensors retain all intellectual property
- * and proprietary rights in and to this software, related documentation
- * and any modifications thereto.  Any use, reproduction, disclosure or
- * distribution of this software and related documentation without an express
- * license agreement from NVIDIA Corporation is strictly prohibited.
- */
+/* SPDX-License-Identifier: GPL-2.0-only */
+/* SPDX-FileCopyrightText: Copyright (c) 2024, NVIDIA CORPORATION & AFFILIATES.  All rights reserved. */
 
 // Definitions for tegra234 android bootargs
 
