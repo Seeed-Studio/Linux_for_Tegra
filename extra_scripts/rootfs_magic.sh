affc8204d04d5862a9700aa265ce82ef23202dc0
jian xiong <953308023@qq.com>
2025-03-10 02:07:27 +0000

    install dkms driver for j40mini

--- extra_scripts/rootfs_magic.sh
+++ extra_scripts/rootfs_magic.sh
@@ -3,6 +3,7 @@ echo 'Acquire::http::Proxy "http://192.168.1.77:3142";' >> /etc/apt/apt.conf
 
 apt-get update
 apt install -y nvidia-jetpack pulseaudio-module-bluetooth
+apt install -y rtl8821ce-dkms
 systemctl disabled nvgetty.service
 apt-get clean
 rm /etc/apt/apt.conf
