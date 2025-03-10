8895bf14e8f8b28935969e6cf8378e76d346d7b8
Zhiyuan Wang <zhiwang@nvidia.com>
2025-03-09 20:03:20 -0700

    linux: kmd: fix the bug TOP25 03/05 Part 9
    Add dev-err when failure:
    - drivers/media/platform/tegra/camera/vi/mc_common.c
    
    Jira CAMERASW-32689
    
    Change-Id: I87dc4593fe91889406a8d61843d260e953a2a1c2
    Signed-off-by: Zhiyuan Wang <zhiwang@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/c/linux-nv-oot/+/3316291
    GVS: buildbot_gerritrpt <buildbot_gerritrpt@nvidia.com>
    Reviewed-by: Semi Malinen <smalinen@nvidia.com>
    Reviewed-by: Ajith Kumar <ajithk@nvidia.com>
    Reviewed-by: Frank Chen <frankc@nvidia.com>

--- drivers/media/platform/tegra/camera/vi/mc_common.c
+++ drivers/media/platform/tegra/camera/vi/mc_common.c
@@ -223,9 +223,11 @@ int tpg_vi_media_controller_init(struct tegra_mc_vi *mc_vi, int pg_mode)
 	mc_vi->tpg_start = NULL;
 	for (i = 0; i < mc_vi->csi->num_tpg_channels; i++) {
 		item = devm_kzalloc(mc_vi->dev, sizeof(*item), GFP_KERNEL);
-		if (!item)
+		if (!item) {
+			dev_err(mc_vi->dev, "failed to allocate channel\n");
+			err = -ENOMEM;
 			goto channel_init_error;
-
+		}
 
 		if ((__builtin_add_overflow(num_pre_channels, i, &item->id))) {
 			devm_kfree(mc_vi->dev, item);
