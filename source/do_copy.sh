1d9c68681b7987efe72f2fa26aafc2699ae6d1cf
jian xiong <953308023@qq.com>
2025-03-19 02:36:31 +0000

    upload dtb files and relative files for recomputer super j401

--- source/do_copy.sh
+++ source/do_copy.sh
@@ -27,6 +27,12 @@ cp ./kernel_out/kernel-devicetree/generic-dts/dtbs/tegra234-j40mini-p3768-0000+p
 cp ./kernel_out/kernel-devicetree/generic-dts/dtbs/tegra234-j40mini-p3768-0000+p3767-0003-recomputer.dtb ../kernel/dtb/
 cp ./kernel_out/kernel-devicetree/generic-dts/dtbs/tegra234-j40mini-p3768-0000+p3767-0004-recomputer.dtb ../kernel/dtb/
 
+cp ./kernel_out/kernel-devicetree/generic-dts/dtbs/tegra234-j401-p3768-0000+p3767-0000-recomputer-super.dtb ../kernel/dtb/
+cp ./kernel_out/kernel-devicetree/generic-dts/dtbs/tegra234-j401-p3768-0000+p3767-0001-recomputer-super.dtb ../kernel/dtb/
+cp ./kernel_out/kernel-devicetree/generic-dts/dtbs/tegra234-j401-p3768-0000+p3767-0003-recomputer-super.dtb ../kernel/dtb/
+cp ./kernel_out/kernel-devicetree/generic-dts/dtbs/tegra234-j401-p3768-0000+p3767-0004-recomputer-super.dtb ../kernel/dtb/
+
 cp ./kernel_out/kernel-devicetree/generic-dts/dtbs/tegra234-p3767-camera-p3768-imx219-dual-seeed.dtbo ../kernel/dtb/
+cp ./kernel_out/kernel-devicetree/generic-dts/dtbs/tegra234-p3767-camera-p3768-imx219-quad-seeed.dtbo ../kernel/dtb/
 
 cp  ./kernel_out/kernel/kernel-jammy-src/arch/arm64/boot/Image ../kernel/Image
