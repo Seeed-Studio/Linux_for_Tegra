3e5e90335f09916dbafdce9b36abf70631782f6a
Jon Hunter <jonathanh@nvidia.com>
2025-04-14 11:00:27 +0100

    NVIDIA: SAUCE: Revert "NVIDIA: SAUCE: phy: tegra: Prevent bias pad refcount mismatch during tegra suspend"
    This reverts commit 8d5fab5fc9837d0315011fbc7c81b3e17257fa13 to prepare
    for integrating the fix accepted upstream.
    
    http://nvbugs/5145954
    
    Signed-off-by: Jon Hunter <jonathanh@nvidia.com>
    Reviewed-by: Wayne Chang <waynec@nvidia.com>
    Tested-by: Wayne Chang <waynec@nvidia.com>
    Signed-off-by: Kartik Rajput <kkartik@nvidia.com>
    Acked-by: Noah Wager <noah.wager@canonical.com>
    Acked-by: Jacob Martin <jacob.martin@canonical.com>
    Signed-off-by: Noah Wager <noah.wager@canonical.com>

--- drivers/phy/tegra/xusb-tegra186.c
+++ drivers/phy/tegra/xusb-tegra186.c
@@ -734,7 +734,6 @@ static void tegra186_utmi_bias_pad_power_off(struct tegra_xusb_padctl *padctl)
 static void tegra186_utmi_pad_power_on(struct phy *phy)
 {
 	struct tegra_xusb_lane *lane = phy_get_drvdata(phy);
-	struct tegra_xusb_usb2_lane *usb2 = to_usb2_lane(lane);
 	struct tegra_xusb_padctl *padctl = lane->pad->padctl;
 	struct tegra_xusb_usb2_port *port;
 	struct device *dev = padctl->dev;
@@ -750,9 +749,6 @@ static void tegra186_utmi_pad_power_on(struct phy *phy)
 		return;
 	}
 
-	if (usb2->powered_on)
-		return;
-
 	dev_dbg(dev, "power on UTMI pad %u\n", index);
 
 	tegra186_utmi_bias_pad_power_on(padctl);
@@ -766,22 +762,16 @@ static void tegra186_utmi_pad_power_on(struct phy *phy)
 	value = padctl_readl(padctl, XUSB_PADCTL_USB2_OTG_PADX_CTL1(index));
 	value &= ~USB2_OTG_PD_DR;
 	padctl_writel(padctl, value, XUSB_PADCTL_USB2_OTG_PADX_CTL1(index));
-
-	usb2->powered_on = true;
 }
 
 static void tegra186_utmi_pad_power_down(struct phy *phy)
 {
 	struct tegra_xusb_lane *lane = phy_get_drvdata(phy);
-	struct tegra_xusb_usb2_lane *usb2 = to_usb2_lane(lane);
 	struct tegra_xusb_padctl *padctl = lane->pad->padctl;
 	unsigned int index = lane->index;
 	u32 value;
 
 	if (!phy)
-		return;
-
-	if (!usb2->powered_on)
 		return;
 
 	dev_dbg(padctl->dev, "power down UTMI pad %u\n", index);
@@ -797,8 +787,6 @@ static void tegra186_utmi_pad_power_down(struct phy *phy)
 	udelay(2);
 
 	tegra186_utmi_bias_pad_power_off(padctl);
-
-	usb2->powered_on = false;
 }
 
 static int tegra186_xusb_padctl_vbus_override(struct tegra_xusb_padctl *padctl,
