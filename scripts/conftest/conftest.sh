b578f490b693083e9c3fbff4af921be04a625fd6
Jon Hunter <jonathanh@nvidia.com>
2025-04-17 10:59:40 +0100

    conftest: Fix return types for timer functions
    The function hrtimer_setup() returns void and the function
    timer_delete() returns int, and so correct the return types
    for the respective conftest functions.
    
    JIRA LINQPJ14-47
    
    Change-Id: I569f4fae0bf01f894583570116125a44a1757ff3
    Signed-off-by: Jon Hunter <jonathanh@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/c/linux-nv-oot/+/3342808
    Reviewed-by: svcacv <svcacv@nvidia.com>
    GVS: buildbot_gerritrpt <buildbot_gerritrpt@nvidia.com>
    Reviewed-by: Brad Griffis <bgriffis@nvidia.com>

--- scripts/conftest/conftest.sh
+++ scripts/conftest/conftest.sh
@@ -7691,8 +7691,8 @@ compile_test() {
             #
             CODE="
             #include <linux/hrtimer.h>
-            bool conftest_hrtimer_setup(void) {
-                return hrtimer_setup();
+            void conftest_hrtimer_setup(void) {
+                hrtimer_setup();
             }"
 
             compile_check_conftest "$CODE" "NV_HRTIMER_SETUP_PRESENT" "" "functions"
@@ -8705,7 +8705,7 @@ compile_test() {
             #
             CODE="
             #include <linux/timer.h>
-            bool conftest_timer_delete(void) {
+            int conftest_timer_delete(void) {
                 return timer_delete();
             }"
 
