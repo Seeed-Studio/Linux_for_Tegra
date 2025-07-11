a7fae6153ab6eb953cbec5477601e51eea9f657b
Shobek Attupurath <sattupurath@nvidia.com>
2025-07-11 01:15:48 +0000

    rtl8852ce: Add Nvidia changes to driver version 261-1
    - Reduce log level
    - Remove unwanted prints
    - Add Nvidia sourcetree path
    
    Bug 5207091
    
    Change-Id: I1fa512ea04ef703d2f17608658abf2e258c38db3
    Signed-off-by: Shobek Attupurath <sattupurath@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/c/linux-nv-oot/+/3415008
    Reviewed-by: Bibek Basu <bbasu@nvidia.com>

--- drivers/net/wireless/realtek/rtl8852ce/phl/phl.mk
+++ drivers/net/wireless/realtek/rtl8852ce/phl/phl.mk
@@ -11,10 +11,10 @@ endif
 
 ifeq ($(CONFIG_PHL_ARCH), y)
 phl_path := phl/
-phl_path_d1 := $(src)/phl/$(HAL)
+phl_path_d1 := $(SOURCETREE)/phl/$(HAL)
 else
 phl_path :=
-phl_path_d1 := $(src)/$(HAL)
+phl_path_d1 := $(SOURCETREE)/$(HAL)
 endif
 
 _PHL_FILES := $(phl_path)phl_init.o \
