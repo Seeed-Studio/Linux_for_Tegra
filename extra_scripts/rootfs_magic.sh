592edddebef52976ddcc9fc5c77158d1c4650e68
shuishengpeng <1143590135@qq.com>
2025-07-22 14:25:23 +0800

    Migrate configuration from seeed jecpack 36.4.3

---
+++ extra_scripts/rootfs_magic.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -x
+ 
+echo 'Acquire::http::Proxy "http://192.168.1.77:3142";' >> /etc/apt/apt.conf
+
+apt-get update
+apt install -y nvidia-jetpack
+apt install -y pulseaudio-module-bluetooth
+systemctl disable nvgetty.service
+apt-get clean
+rm /etc/apt/apt.conf
+
+dpkg -i \
+  /glib/libglib2.0-0_2.72.4-0ubuntu2.4~portwell1_arm64.deb \
+  /glib/libglib2.0-bin_2.72.4-0ubuntu2.4~portwell1_arm64.deb \
+  /glib/libglib2.0-data_2.72.4-0ubuntu2.4~portwell1_all.deb
+
+dpkg -i \
+  /glib/libglib2.0-dev_2.72.4-0ubuntu2.4~portwell1_arm64.deb \
+  /glib/libglib2.0-dev-bin_2.72.4-0ubuntu2.4~portwell1_arm64.deb \
+  /glib/libglib2.0-doc_2.72.4-0ubuntu2.4~portwell1_all.deb \
+  /glib/libglib2.0-tests_2.72.4-0ubuntu2.4~portwell1_arm64.deb
---
+++ source/do_copy.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+cp ./kernel_out/kernel-devicetree/generic-dts/dtbs/tegra234-j501x-0000+p3701-0004-reserver.dtb ../kernel/dtb/
+cp ./kernel_out/kernel-devicetree/generic-dts/dtbs/tegra234-j501x-0000+p3701-0005-reserver.dtb ../kernel/dtb/
+cp ./kernel_out/kernel-devicetree/generic-dts/dtbs/tegra234-j501x-0000+p3701-0000-reserver.dtb ../kernel/dtb/
+cp ./kernel_out/kernel-devicetree/generic-dts/dtbs/tegra234-j501x-0000+p3701-0004-reserver-gmsl.dtb ../kernel/dtb/
+cp ./kernel_out/kernel-devicetree/generic-dts/dtbs/tegra234-j501x-0000+p3701-0005-reserver-gmsl.dtb ../kernel/dtb/
+cp ./kernel_out/kernel-devicetree/generic-dts/dtbs/tegra234-j501x-0000+p3701-0000-reserver-gmsl.dtb ../kernel/dtb/
+
+cp ./kernel_out/kernel-devicetree/generic-dts/dtbs/tegra234-j401-p3768-0000+p3767-0000-recomputer.dtb ../kernel/dtb/
+cp ./kernel_out/kernel-devicetree/generic-dts/dtbs/tegra234-j401-p3768-0000+p3767-0001-recomputer.dtb ../kernel/dtb/
+cp ./kernel_out/kernel-devicetree/generic-dts/dtbs/tegra234-j401-p3768-0000+p3767-0003-recomputer.dtb ../kernel/dtb/
+cp ./kernel_out/kernel-devicetree/generic-dts/dtbs/tegra234-j401-p3768-0000+p3767-0004-recomputer.dtb ../kernel/dtb/
+
+cp ./kernel_out/kernel-devicetree/generic-dts/dtbs/tegra234-j401-p3768-0000+p3767-0000-reserver-indu.dtb ../kernel/dtb/
+cp ./kernel_out/kernel-devicetree/generic-dts/dtbs/tegra234-j401-p3768-0000+p3767-0001-reserver-indu.dtb ../kernel/dtb/
+cp ./kernel_out/kernel-devicetree/generic-dts/dtbs/tegra234-j401-p3768-0000+p3767-0003-reserver-indu.dtb ../kernel/dtb/
+cp ./kernel_out/kernel-devicetree/generic-dts/dtbs/tegra234-j401-p3768-0000+p3767-0004-reserver-indu.dtb ../kernel/dtb/
+
+cp ./kernel_out/kernel-devicetree/generic-dts/dtbs/tegra234-j201-p3768-0000+p3767-0000-recomputer-indu.dtb ../kernel/dtb/
+cp ./kernel_out/kernel-devicetree/generic-dts/dtbs/tegra234-j201-p3768-0000+p3767-0001-recomputer-indu.dtb ../kernel/dtb/
+cp ./kernel_out/kernel-devicetree/generic-dts/dtbs/tegra234-j201-p3768-0000+p3767-0003-recomputer-indu.dtb ../kernel/dtb/
+cp ./kernel_out/kernel-devicetree/generic-dts/dtbs/tegra234-j201-p3768-0000+p3767-0004-recomputer-indu.dtb ../kernel/dtb/
+
+cp ./kernel_out/kernel-devicetree/generic-dts/dtbs/tegra234-j40mini-p3768-0000+p3767-0000-recomputer.dtb ../kernel/dtb/
+cp ./kernel_out/kernel-devicetree/generic-dts/dtbs/tegra234-j40mini-p3768-0000+p3767-0001-recomputer.dtb ../kernel/dtb/
+cp ./kernel_out/kernel-devicetree/generic-dts/dtbs/tegra234-j40mini-p3768-0000+p3767-0003-recomputer.dtb ../kernel/dtb/
+cp ./kernel_out/kernel-devicetree/generic-dts/dtbs/tegra234-j40mini-p3768-0000+p3767-0004-recomputer.dtb ../kernel/dtb/
+
+cp ./kernel_out/kernel-devicetree/generic-dts/dtbs/tegra234-j401-p3768-0000+p3767-0000-recomputer-super.dtb ../kernel/dtb/
+cp ./kernel_out/kernel-devicetree/generic-dts/dtbs/tegra234-j401-p3768-0000+p3767-0001-recomputer-super.dtb ../kernel/dtb/
+cp ./kernel_out/kernel-devicetree/generic-dts/dtbs/tegra234-j401-p3768-0000+p3767-0003-recomputer-super.dtb ../kernel/dtb/
+cp ./kernel_out/kernel-devicetree/generic-dts/dtbs/tegra234-j401-p3768-0000+p3767-0004-recomputer-super.dtb ../kernel/dtb/
+
+cp ./kernel_out/kernel-devicetree/generic-dts/dtbs/tegra234-j401-p3768-0000+p3767-0000-recomputer-robo.dtb ../kernel/dtb/
+cp ./kernel_out/kernel-devicetree/generic-dts/dtbs/tegra234-j401-p3768-0000+p3767-0001-recomputer-robo.dtb ../kernel/dtb/
+cp ./kernel_out/kernel-devicetree/generic-dts/dtbs/tegra234-j401-p3768-0000+p3767-0003-recomputer-robo.dtb ../kernel/dtb/
+cp ./kernel_out/kernel-devicetree/generic-dts/dtbs/tegra234-j401-p3768-0000+p3767-0004-recomputer-robo.dtb ../kernel/dtb/
+
+cp ./kernel_out/kernel-devicetree/generic-dts/dtbs/tegra234-p3767-camera-p3768-imx219-dual-seeed.dtbo ../kernel/dtb/
+cp ./kernel_out/kernel-devicetree/generic-dts/dtbs/tegra234-p3767-camera-p3768-imx219-quad-seeed.dtbo ../kernel/dtb/
+cp ./kernel_out/kernel-devicetree/generic-dts/dtbs/tegra234-p3767-camera-p3768-imx477-dual-seeed.dtbo ../kernel/dtb/
+
+cp ./kernel_out/kernel-devicetree/generic-dts/dtbs/tegra234-seeed-gmsl2x4-3g-overlay.dtbo ../kernel/dtb/
+cp ./kernel_out/kernel-devicetree/generic-dts/dtbs/tegra234-seeed-gmsl1x4-6g-overlay.dtbo ../kernel/dtb/
+cp ./kernel_out/kernel-devicetree/generic-dts/dtbs/tegra234-seeed-gmsl1x4-3g-overlay.dtbo ../kernel/dtb/
+cp ./kernel_out/kernel-devicetree/generic-dts/dtbs/tegra234-seeed-orbbec-335lg-overlay.dtbo ../kernel/dtb/
+
+
+cp ./kernel_out/kernel-devicetree/generic-dts/dtbs/tegra234-j401-p3768-0000+p3767-0000-recomputer-robo-gmsl.dtb ../kernel/dtb/
+cp ./kernel_out/kernel-devicetree/generic-dts/dtbs/tegra234-j401-p3768-0000+p3767-0001-recomputer-robo-gmsl.dtb ../kernel/dtb/
+cp ./kernel_out/kernel-devicetree/generic-dts/dtbs/tegra234-j401-p3768-0000+p3767-0003-recomputer-robo-gmsl.dtb ../kernel/dtb/
+cp ./kernel_out/kernel-devicetree/generic-dts/dtbs/tegra234-j401-p3768-0000+p3767-0004-recomputer-robo-gmsl.dtb ../kernel/dtb/
+
+
+
+
+cp  ./kernel_out/kernel/kernel-jammy-src/arch/arm64/boot/Image ../kernel/Image
