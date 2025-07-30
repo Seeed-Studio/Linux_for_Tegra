f0be6725022cacee07ee812eca3b6db1aecb3bc4
Tang Ruiqian <ruiqian.tang@seeed.cc>
2025-07-30 09:53:27 +0800

    add recomputer-mini-agx-orin-j501

--- source/do_copy.sh
+++ source/do_copy.sh
@@ -53,6 +53,12 @@ cp ./kernel_out/kernel-devicetree/generic-dts/dtbs/tegra234-j401-p3768-0000+p376
 cp ./kernel_out/kernel-devicetree/generic-dts/dtbs/tegra234-j401-p3768-0000+p3767-0004-recomputer-robo-gmsl.dtb ../kernel/dtb/
 
 
+cp ./kernel_out/kernel-devicetree/generic-dts/dtbs/tegra234-j501x-0000+p3701-0000-recomputer-mini.dtb ../kernel/dtb/
+cp ./kernel_out/kernel-devicetree/generic-dts/dtbs/tegra234-j501x-0000+p3701-0004-recomputer-mini.dtb ../kernel/dtb/
+cp ./kernel_out/kernel-devicetree/generic-dts/dtbs/tegra234-j501x-0000+p3701-0005-recomputer-mini.dtb ../kernel/dtb/
 
+cp ./kernel_out/kernel-devicetree/generic-dts/dtbs/tegra234-j501x-0000+p3701-0000-recomputer-mini.dtb ../kernel/dtb/
+cp ./kernel_out/kernel-devicetree/generic-dts/dtbs/tegra234-j501x-0000+p3701-0004-recomputer-mini.dtb ../kernel/dtb/
+cp ./kernel_out/kernel-devicetree/generic-dts/dtbs/tegra234-j501x-0000+p3701-0005-recomputer-mini.dtb ../kernel/dtb/
 
 cp  ./kernel_out/kernel/kernel-jammy-src/arch/arm64/boot/Image ../kernel/Image
