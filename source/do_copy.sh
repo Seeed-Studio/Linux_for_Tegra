41b0bdb0ceb5d073325b3e5f719f30b685002844
Tang Ruiqian <ruiqian.tang@seeed.cc>
2025-10-10 19:36:11 +0800

    recomputer-robo-j501:Add gmsl 3g overlay.

--- source/do_copy.sh
+++ source/do_copy.sh
@@ -48,7 +48,7 @@ cp ./kernel_out/kernel-devicetree/generic-dts/dtbs/tegra234-seeed-gmsl2x1x4-3g-o
 cp ./kernel_out/kernel-devicetree/generic-dts/dtbs/tegra234-seeed-gmsl2x1x4-6g-overlay.dtbo ../kernel/dtb/
 cp ./kernel_out/kernel-devicetree/generic-dts/dtbs/tegra234-seeed-orbbec-335lg-overlay.dtbo ../kernel/dtb/
 cp ./kernel_out/kernel-devicetree/generic-dts/dtbs/tegra234-seeed-gmsl2x4-orbbec-335lg-overlay.dtbo ../kernel/dtb/
-
+cp ./kernel_out/kernel-devicetree/generic-dts/dtbs/tegra234-seeed-gmsl-recomputer-robo-overlay.dtbo ../kernel/dtb/
 
 cp ./kernel_out/kernel-devicetree/generic-dts/dtbs/tegra234-j401-p3768-0000+p3767-0000-recomputer-robo-gmsl.dtb ../kernel/dtb/
 cp ./kernel_out/kernel-devicetree/generic-dts/dtbs/tegra234-j401-p3768-0000+p3767-0001-recomputer-robo-gmsl.dtb ../kernel/dtb/
