0677da13cfac3c35e08059b6e37ec375fdb8f494
ackPeng <1143590135@qq.com>
2025-10-28 13:49:30 +0800

    fix ble && black screen

--- extra_scripts/rootfs_magic.sh
+++ extra_scripts/rootfs_magic.sh
@@ -7,3 +7,14 @@ systemctl disabled nvgetty.service
 apt-get clean
 rm /etc/apt/apt.conf
 
+dpkg -i \
+  /glib/libglib2.0-0_2.72.4-0ubuntu2.4~portwell1_arm64.deb \
+  /glib/libglib2.0-bin_2.72.4-0ubuntu2.4~portwell1_arm64.deb \
+  /glib/libglib2.0-data_2.72.4-0ubuntu2.4~portwell1_all.deb
+
+dpkg -i \
+  /glib/libglib2.0-dev_2.72.4-0ubuntu2.4~portwell1_arm64.deb \
+  /glib/libglib2.0-dev-bin_2.72.4-0ubuntu2.4~portwell1_arm64.deb \
+  /glib/libglib2.0-doc_2.72.4-0ubuntu2.4~portwell1_all.deb \
+  /glib/libglib2.0-tests_2.72.4-0ubuntu2.4~portwell1_arm64.deb
+
