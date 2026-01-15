247fd5ff28d7404bed738f898668435531fc248a
Bohrium332 <2759897880@qq.com>
2026-01-15 14:48:31 +0800

    hold L4T kernel packages to prevent accidental upgrades

--- extra_scripts/rootfs_magic.sh
+++ extra_scripts/rootfs_magic.sh
@@ -20,3 +20,5 @@ dpkg -i \
   /glib/libglib2.0-dev-bin_2.72.4-0ubuntu2.4~portwell1_arm64.deb \
   /glib/libglib2.0-doc_2.72.4-0ubuntu2.4~portwell1_all.deb \
   /glib/libglib2.0-tests_2.72.4-0ubuntu2.4~portwell1_arm64.deb
+
+sudo apt-mark hold nvidia-l4t-display-kernel nvidia-l4t-kernel nvidia-l4t-kernel-dtbs nvidia-l4t-kernel-headers nvidia-l4t-kernel-oot-headers nvidia-l4t-kernel-oot-modules
