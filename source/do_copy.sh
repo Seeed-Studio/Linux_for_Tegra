10c5ff8af85565030aa5534322ab7700ed91feb9
qian <ruiqian.tang@seeed.cc>
2026-03-30 17:39:54 +0800

    recomputer-carrier-j601:add orbbec gmsl 3g.

--- source/do_copy.sh
+++ source/do_copy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 cp ./kernel_out/kernel-devicetree/generic-dts/dtbs/tegra264-p4071-0000+p3834-0008-nv.dtb ../kernel/dtb/
 cp ./kernel_out/kernel-devicetree/generic-dts/dtbs/tegra264-p4071-0000+p3834-0008-recomputer-carrier.dtb ../kernel/dtb/
+cp ./kernel_out/kernel-devicetree/generic-dts/dtbs/tegra264-p4071-camera-* ../kernel/dtb/
 
 cp  ./kernel_out/kernel/kernel-noble/arch/arm64/boot/Image ../kernel/Image
