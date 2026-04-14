c5e5888ea599f799b063c5eac00b8bbe4170ee55
Bohrium332 <2759897880@qq.com>
2026-04-14 13:59:36 +0800

    Migrate configuration from seeed jecpack 36.4.4

---
+++ extra_scripts/rootfs_magic.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+set -x
+ 
+echo 'Acquire::http::Proxy "http://192.168.1.77:3142";' >> /etc/apt/apt.conf
+
+apt-get update
+apt install -y nvidia-jetpack
+apt install -y pulseaudio-module-bluetooth
+systemctl disable nvgetty.service
+apt-get clean
+rm /etc/apt/apt.conf
+
+dpkg -i \
+  /glib/libglib2.0-0_2.72.4-0ubuntu2.4~portwell1_arm64.deb \
+  /glib/libglib2.0-bin_2.72.4-0ubuntu2.4~portwell1_arm64.deb \
+  /glib/libglib2.0-data_2.72.4-0ubuntu2.4~portwell1_all.deb
+
+dpkg -i \
+  /glib/libglib2.0-dev_2.72.4-0ubuntu2.4~portwell1_arm64.deb \
+  /glib/libglib2.0-dev-bin_2.72.4-0ubuntu2.4~portwell1_arm64.deb \
+  /glib/libglib2.0-doc_2.72.4-0ubuntu2.4~portwell1_all.deb \
+  /glib/libglib2.0-tests_2.72.4-0ubuntu2.4~portwell1_arm64.deb
+
+cat > /etc/apt/apt.conf.d/99disable-auto-updates << EOF
+APT::Periodic::Update-Package-Lists "0";
+APT::Periodic::Download-Upgradeable-Packages "0";
+APT::Periodic::AutocleanInterval "0";
+APT::Periodic::Unattended-Upgrade "0";
+EOF
+chattr +i /etc/apt/apt.conf.d/99disable-auto-updates
+
+apt-get remove --purge update-manager update-notifier
+
+sudo apt-mark hold nvidia-l4t-display-kernel nvidia-l4t-kernel nvidia-l4t-kernel-dtbs nvidia-l4t-kernel-headers nvidia-l4t-kernel-oot-headers nvidia-l4t-kernel-oot-modules nvidia-l4t-initrd
