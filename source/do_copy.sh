087c7b49092e3a49a76689dc4419961caa18a46e
Bohrium332 <2759897880@qq.com>
2026-07-27 20:19:33 +0800

    ci: deploy Seeed quad IMX219/IMX477 overlays
    Copy the 4x IMX477 and 2x IMX219/2x IMX477 DTBOs from the R36 build output into the CI rootfs.

--- source/do_copy.sh
+++ source/do_copy.sh
@@ -40,6 +40,8 @@ cp ./kernel_out/kernel-devicetree/generic-dts/dtbs/tegra234-j401-p3768-0000+p376
 cp ./kernel_out/kernel-devicetree/generic-dts/dtbs/tegra234-p3767-camera-p3768-imx219-dual-seeed.dtbo ../kernel/dtb/
 cp ./kernel_out/kernel-devicetree/generic-dts/dtbs/tegra234-p3767-camera-p3768-imx219-quad-seeed.dtbo ../kernel/dtb/
 cp ./kernel_out/kernel-devicetree/generic-dts/dtbs/tegra234-p3767-camera-p3768-imx477-dual-seeed.dtbo ../kernel/dtb/
+cp ./kernel_out/kernel-devicetree/generic-dts/dtbs/tegra234-p3767-camera-p3768-imx477-quad-seeed.dtbo ../kernel/dtb/
+cp ./kernel_out/kernel-devicetree/generic-dts/dtbs/tegra234-p3767-camera-p3768-imx219-imx477-quad-seeed.dtbo ../kernel/dtb/
 cp ./kernel_out/kernel-devicetree/generic-dts/dtbs/tegra234-p3767-camera-p3768-imx219-imx477.dtbo ../kernel/dtb/
 cp ./kernel_out/kernel-devicetree/generic-dts/dtbs/tegra234-p3767-camera-p3768-imx477-imx219.dtbo ../kernel/dtb/
 
