d2906d06c356d3a2917e13d293c470bf878ab98c
Bohrium332 <2759897880@qq.com>
2026-08-12 20:00:08 +0800

    fix: add capabilities for ping command in rootfs magic script

--- extra_scripts/rootfs_magic.sh
+++ extra_scripts/rootfs_magic.sh
@@ -18,3 +18,5 @@ APT::Periodic::Unattended-Upgrade "0";
 EOF
 sudo apt-get remove --purge -y update-manager update-notifier
 sudo apt-mark hold nvidia-l4t-display-kernel nvidia-l4t-kernel nvidia-l4t-kernel-dtbs nvidia-l4t-kernel-headers nvidia-l4t-kernel-oot-headers nvidia-l4t-kernel-oot-modules nvidia-l4t-initrd
+/usr/sbin/setcap cap_net_raw+ep /usr/bin/ping
+/usr/sbin/getcap -v /usr/bin/ping | grep -Fx '/usr/bin/ping cap_net_raw=ep'
