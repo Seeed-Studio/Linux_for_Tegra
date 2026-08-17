50b2e82fd43756466d41699f588e69fa55cfa876
Bohrium332 <2759897880@qq.com>
2026-08-17 13:40:48 +0800

    fix: freeze NVIDIA apt packages to BSP version 39.2.0

--- extra_scripts/rootfs_magic.sh
+++ extra_scripts/rootfs_magic.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-set -x
+set -ex
  
 echo 'Acquire::http::Proxy "http://192.168.1.77:3142";' >> /etc/apt/apt.conf
 
 apt-get update
 apt install -y nvidia-jetpack
 apt install -y pulseaudio-module-bluetooth
-systemctl disable nvgetty.service
+systemctl disable nvgetty.service || true # no nvgetty in R39 rootfs, kept for older BSPs
 apt-get clean
 rm /etc/apt/apt.conf
 
