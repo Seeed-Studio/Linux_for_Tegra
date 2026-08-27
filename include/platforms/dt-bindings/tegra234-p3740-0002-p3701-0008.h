199d7e672041748441dcab31af6fa565f0fb5bb5
Bohrium332 <2759897880@qq.com>
2026-08-27 10:16:44 +0800

    Squashed 'source/hardware/nvidia/t23x/nv-public/' content from commit c69f1a1df67e
    git-subtree-dir: source/hardware/nvidia/t23x/nv-public
    git-subtree-split: c69f1a1df67edbe3d6ca34c6c138c2499fa42d91

---
+++ include/platforms/dt-bindings/tegra234-p3740-0002-p3701-0008.h
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2024, NVIDIA CORPORATION.  All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms and conditions of the GNU General Public License,
+ * version 2, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
+ * Definitions for Jetson tegra234-p3740-0002-p3701-0008 board.
+ */
+
+#include <dt-bindings/gpio/tegra234-gpio.h>
+
+#define JETSON_COMPATIBLE	"nvidia,p3740-0002+p3701-0008"
