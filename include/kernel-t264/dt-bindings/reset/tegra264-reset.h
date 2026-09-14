78f48ccaa56d17df8d74ee9be00e7280d035f898
Bohrium332 <2759897880@qq.com>
2026-09-14 02:46:47 +0000

    Squashed 'source/hardware/nvidia/t264/nv-public/' content from commit cca182464f24
    git-subtree-dir: source/hardware/nvidia/t264/nv-public
    git-subtree-split: cca182464f24f6fded34bd18575131a3d4a56b3e

---
+++ include/kernel-t264/dt-bindings/reset/tegra264-reset.h
@@ -0,0 +1,9 @@
+/* SPDX-License-Identifier: (GPL-2.0-only OR BSD-2-Clause) */
+/* SPDX-FileCopyrightText: Copyright (c) 2023-2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved. */
+
+#ifndef _DT_BINDINGS_RESET_TEGRA264_RESET_H
+#define _DT_BINDINGS_RESET_TEGRA264_RESET_H
+
+#include <dt-bindings/reset/tegra264-rst.h>
+
+#endif
